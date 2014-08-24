cc46812df38b44d8a1f6c82cc3e609bac2ab8be2
Oliver <Oliver@rpi-controlcenter.de>
2014-08-24 08:27:49 +0200

    initial Commit

---
+++ rwf/global.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Grundeinstellungen des Frameworks
+ * 
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+
+//Globale Konstanten
+define('PATH_BASE', dirname(__DIR__)); //Pfad zum Hauptordner der Anwendung
+define('PATH_RWF', __DIR__);           //Pfad zum Hauptordner des Frameworks
+
