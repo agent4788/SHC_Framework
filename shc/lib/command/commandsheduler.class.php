063e727c7f1db628156297b3cdd571d34dc3babc
Oliver <Oliver@rpi-controlcenter.de>
2014-09-15 06:15:01 +0200

    Senden von Funksteckdosen- und GpioOutput-Kommandos an die Schaltserver implementiert

--- shc/lib/command/commandsheduler.class.php
+++ shc/lib/command/commandsheduler.class.php
@@ -3,7 +3,9 @@
 namespace SHC\Command;
 
 //Imports
-
+use SHC\SwitchServer\SwitchServerEditor;
+use SHC\Command\Commands\RadioSocketCommand;
+use SHC\Command\Commands\GpioOutputCommand;
 
 /**
  * Verwaltet und sendet die Kommandos an die Steckdosen/GPIOs
@@ -15,21 +17,21 @@ namespace SHC\Command;
  * @version    2.0.0-0
  */
 class CommandSheduler {
-    
+
     /**
      * liste mit allen Kommandos
      * 
      * @var Array 
      */
     protected $commands = array();
-    
+
     /**
      * Singleton Instanz
      * 
      * @var \SHC\Command\CommandSheduler
      */
     protected static $instance = null;
-    
+
     /**
      * fuegt ein neues Kommando hinzu
      * 
@@ -37,11 +39,11 @@ class CommandSheduler {
      * @return \SHC\Command\CommandSheduler
      */
     public function addCommand(Command $command) {
-        
+
         $this->commands[] = $command;
         return $this;
     }
-    
+
     /**
      * entfernt ein Kommando
      * 
@@ -49,29 +51,70 @@ class CommandSheduler {
      * @return \SHC\Command\CommandSheduler
      */
     public function removeCommand(Command $command) {
-        
+
         $this->commands = array_diff($this->commands, array($command));
         return $this;
     }
-    
+
     /**
      * entfernt alle Kommandos
      * 
      * @return \SHC\Command\CommandSheduler
      */
     public function removeAllCommands() {
-        
+
         $this->commands = array();
         return $this;
     }
-    
+
     /**
      * Sendet die Kommandos an den jeweiligen Schaltserver
      */
     public function sendCommands() {
-        
+
+        //Schaltserver abfragen
+        $switchServers = SwitchServerEditor::getInstance()->listSwitchServers();
+
+        //alle Server durchlaufen und die Daten senden
+        foreach ($switchServers as $switchServer) {
+
+            /* @var $switchServer \SHC\SwitchServer\SwitchServer */
+            //alle Kommandos durchlaufen und Daten Aufbereiten
+            foreach ($this->commands as $command) {
+
+                //Request Initialisieren
+                $request = array();
+
+                /* @var $command \SHC\Command\Command */
+                if ($command instanceof RadioSocketCommand) {
+
+                    //Funksteckdose
+                    if ($switchServer->isRadioSocketsEnabled()) {
+
+                        $request[] = $command->getCommandData();
+                    }
+                } elseif ($command instanceof GpioOutputCommand) {
+
+                    //GPIO schalten
+                    if ($switchServer->isWriteGpiosEnabled() && $switchServer->getId() == $command->getSwitchServer()) {
+
+                        $request[] = $command->getCommandData();
+                    }
+                }
+            }
+            
+            //Daten zum versenden aufbereiten
+            $data = json_encode($request);
+            $data = base64_encode($data);
+            
+            //mit Schalserver verbinden und Daten senden
+            $socket = $switchServer->getSocket();
+            $socket->open();
+            $socket->write($data);
+            $socket->close();
+        }
     }
-    
+
     /**
      * geschuetzt wegen Singleton
      */
@@ -92,4 +135,5 @@ class CommandSheduler {
         }
         return self::$instance;
     }
+
 }
