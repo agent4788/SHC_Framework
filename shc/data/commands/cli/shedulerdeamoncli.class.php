1e7d8bfba5d0557c52ddb4810913de97e0fa7216
Oliver <Oliver@rpi-controlcenter.de>
2014-09-21 09:28:03 +0200

    Sheduler implementiert

---
+++ shc/data/commands/cli/shedulerdeamoncli.class.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SHC\Command\CLI;
+
+//Imports
+use RWF\Request\Commands\CliCommand;
+use SHC\Sheduler\Sheduler;
+
+/**
+ * Schaltserver
+ * 
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+class ShedulerDeamonCli extends CliCommand {
+
+    /**
+     * kurzer Kommandozeilen Parameter
+     * 
+     * @var String 
+     */
+    protected $shortParam = '-sh';
+
+    /**
+     * voller Kommandozeilen Parameter
+     * 
+     * @var String 
+     */
+    protected $fullParam = '--sheduler';
+
+    /**
+     * Debug Modus aktiv
+     * 
+     * @var Boolean 
+     */
+    protected $debug = false;
+
+    /**
+     * gibt die Hilfe zu der Kommandozeilen Funktion auf die Kommandozeile aus
+     */
+    public function writeHelp() {
+        
+    }
+
+    /**
+     * konfiguriert das CLI Kommando
+     */
+    protected function config() {
+        
+    }
+
+    /**
+     * fuehrt das CLI Kommando aus
+     */
+    protected function executeCliCommand() {
+
+        //Sheduler Initialisieren
+        $sheduler = new Sheduler();
+
+        //Aufgaben zyklisch ausfuehren
+        while (true) {
+            
+            $sheduler->executeTasks();
+
+            //Ruhezeut bis zum naechsten Durchlauf
+            sleep(5);
+        }
+    }
+
+}
---
+++ shc/lib/sheduler/sheduler.class.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace SHC\Sheduler;
+
+//Imports
+
+/**
+ * fuehrt Nacheinander alle Aufgaben (Tasks) aus
+ * 
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+class Sheduler {
+
+    /**
+     * Liste mit den Aufgaben
+     * 
+     * @var type 
+     */
+    protected $tasks = array();
+
+    public function __construct() {
+
+        $this->loadTasks();
+    }
+
+    /**
+     * laedt die einzelnen Aufgabe aus dem Dateisystem
+     * 
+     * @throws \Exception
+     */
+    public function loadTasks() {
+
+        $path = PATH_SHC_CLASSES . 'sheduler/tasks/';
+        $dir = opendir($path);
+
+        //Dateien Laden und Objekte Initialisieren
+        while ($file = readdir($dir)) {
+
+            if (preg_match('#\.class\.php$#', $file)) {
+
+                //Datei Includieren
+                require_once($path . $file);
+                
+                $class = '\\SHC\\Sheduler\\Tasks\\'. str_replace('.class.php', '', $file);
+                $object = new $class();
+                
+                if($object instanceof Task) {
+                    
+                    //Objekt nach Prioritaet Speichern
+                    if(!isset($this->tasks[$object->getPriority()])) {
+                        
+                        $this->tasks[$object->getPriority()] = array();
+                    }
+                    $this->tasks[$object->getPriority()][] = $object;
+                } else {
+                    
+                    throw new Exception('Die Aufgabe muss die "Task" Schnittstelle implementieren', 1508);
+                }
+            }
+        }
+
+        closedir($dir);
+        
+        //Nach Prioritaeten sortieren
+        ksort($this->tasks, SORT_NUMERIC);
+    }
+
+    /**
+     * fuehrt in der Reihenfolge der Prioritaet die einzelnen Aufgaben aus
+     * 
+     * @throws \Exception
+     */
+    public function executeTasks() {
+        
+        //Prioritaeten durchlaufen
+        foreach($this->tasks as $prio => $objects) {
+            
+            //Aufgaben nacheinander ausfuehren
+            foreach($objects as $task) {
+                
+                /* @var $task \SHC\Sheduler\Task */
+                $task->execute();
+            }
+        }
+    }
+
+}
