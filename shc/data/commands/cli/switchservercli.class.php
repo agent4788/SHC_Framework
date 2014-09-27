54bc9708a70deb7720b97a08441a76952e316fbb
Oliver <Oliver@rpi-controlcenter.de>
2014-09-27 07:26:46 +0200

    Schaltserver in eine eigene Klasse ausgelagert

--- shc/data/commands/cli/switchservercli.class.php
+++ shc/data/commands/cli/switchservercli.class.php
@@ -5,7 +5,7 @@ namespace SHC\Command\CLI;
 //Imports
 use RWF\Core\RWF;
 use RWF\Request\Commands\CliCommand;
-use RWF\IO\SocketServer;
+use SHC\SwitchServer\SwitchServerSocket;
 use RWF\IO\Socket;
 
 /**
@@ -121,107 +121,8 @@ class SwitchServerCli extends CliCommand {
      */
     protected function executeCliCommand() {
 
-        //Einstellungen laden
-        $sendPath = RWF::getSetting('shc.switchServer.sendCommand');
-        $gpioPath = RWF::getSetting('shc.switchServer.gpioCommand');
-        $sendGpio = RWF::getSetting('shc.switchServer.sendLedPin');
-        $senderActive = RWF::getSetting('shc.switchServer.senderActive');
-        $writeGPIO = RWF::getSetting('shc.switchServer.writeGpio');
-        $readGPIO = RWF::getSetting('shc.switchServer.readGpio');
-
-        //GPIO's vorbereiten
-        $gpioModeSet = array();
-
-        //Server initialisieren
-        $server = new SocketServer(RWF::getSetting('shc.switchServer.ip'), RWF::getSetting('shc.switchServer.port'));
-        $server->startServer();
-
-        //Startmeldung
-        $this->response->writeLnColored(RWF::getLanguage()->get('switchServer.startedSuccessfully', RWF::getSetting('shc.switchServer.ip'), RWF::getSetting('shc.switchServer.port')), 'green');
-
-        //GPIO fuer die Status LED initalisieren
-        if ($sendGpio >= 0) {
-
-            @shell_exec($gpioPath . ' mode ' . $sendGpio . ' out');
-        }
-
-        //auf Anfragen warten
-        while (true) {
-
-            //Anfragen vom Server holen
-            $client = $server->accept();
-            $rawData = base64_decode($client->read(8192));
-            $requests = json_decode($rawData, true);
-
-            //Status LED ein
-            if ($sendGpio >= 0) {
-
-                @shell_exec($gpioPath . ' write ' . $sendGpio . ' 1');
-            }
-
-            //Anfragen bearbeiten
-            foreach ($requests as $request) {
-
-                if ($senderActive && isset($request['type']) && $request['type'] = 'radiosocket') {
-
-                    //Funksteckdosen schalten
-                    @shell_exec('sudo ' . $sendPath . ' ' . $request['protocol'] . ' ' . $request['systemCode'] . ' ' . $request['deviceCode'] . ' ' . $request['command']);
-
-                    //Debug ausgabe
-                    if ($this->debug) {
-
-                        $this->response->writeLnColored('sudo ' . $sendPath . ' ' . $request['protocol'] . ' ' . $request['systemCode'] . ' ' . $request['deviceCode'] . ' ' . $request['command'], 'light_blue');
-                    }
-                } elseif ($writeGPIO && isset($request['type']) && $request['type'] = 'gpiooutput') {
-
-                    //GPIO schreiben
-                    //Modus setzen
-                    if (!in_array($gpio['pinNumber'], $gpioModeSet)) {
-
-                        @shell_exec($gpioPath . ' mode ' . $request['pinNumber'] . ' out');
-                        if ($this->debug) {
-
-                            $this->response->writeLnColored($gpioPath . ' mode ' . $request['pinNumber'] . ' out', 'light_green');
-                        }
-                        $gpioModeSet[] = $gpio['pinNumber'];
-                    }
-
-                    //Befehl ausfuehren
-                    @shell_exec($gpioPath . ' write ' . $request['pinNumber'] . ' ' . $request['command']);
-                    if ($this->debug) {
-
-                        $this->response->writeLnColored($gpioPath . ' write ' . $request['pinNumber'] . ' ' . $request['command'], 'light_green');
-                    }
-                } elseif ($readGPIO && isset($request['type']) && $request['type'] = 'gpioinput') {
-
-                    //GPIO lesen
-                    //noch nicht implementiert
-                } elseif (isset($request['stop']) && $request['stop'] == 1) {
-
-                    //Server Stoppen
-                    $client->close();
-                    $server->stopServer();
-                    $this->response->writeLnColored(RWF::getLanguage()->get('switchServer.stoppedSuccessfully'), 'green');
-
-                    //Status LED aus
-                    if ($sendGpio >= 0) {
-
-                        @shell_exec($gpioPath . ' write ' . $sendGpio . ' 0');
-                    }
-                    
-                    return;
-                }
-            }
-
-            //Status LED aus
-            if ($sendGpio >= 0) {
-
-                @shell_exec($gpioPath . ' write ' . $sendGpio . ' 0');
-            }
-
-            //bearbeitung abgeschlossen
-            $client->close();
-        }
+        $switchServer = new SwitchServerSocket();
+        $switchServer->run($this->response);
     }
 
 }
---
+++ shc/lib/switchserver/switchserversocket.class.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace SHC\SwitchServer;
+
+//Imports
+use RWF\Core\RWF;
+use RWF\IO\SocketServer;
+use RWF\IO\SocketServerClient;
+use RWF\Request\CliResponse;
+
+/**
+ * Schaltserver
+ * 
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+class SwitchServerSocket {
+
+    /**
+     * GPIO Modus
+     * 
+     * @var Array 
+     */
+    protected $gpioModeSet = array();
+
+    /**
+     * Antwortobjekt
+     * 
+     * @var \RWF\Request\CliResponse
+     */
+    protected $response = null;
+
+    /**
+     * Debug Ausgabe
+     * 
+     * @var Boolean
+     */
+    protected $debug = false;
+
+    /**
+     * Server Objekt
+     * 
+     * @var RWF\IO\SocketServer 
+     */
+    protected $server = null;
+
+    /**
+     * startet den Server und wartet auf Anfragen
+     * 
+     * @param \RWF\Request\CliResponse $response Antwortobjekt
+     * @param Boolean                  $debug    Debug Meldungen ausgeben
+     * @return type
+     */
+    public function run(CliResponse $response, $debug = false) {
+
+        $this->response = $response;
+        $this->debug = $debug;
+
+        //Einstellungen laden
+        $sendPath = RWF::getSetting('shc.switchServer.sendCommand');
+        $gpioPath = RWF::getSetting('shc.switchServer.gpioCommand');
+        $sendGpio = RWF::getSetting('shc.switchServer.sendLedPin');
+        $senderActive = RWF::getSetting('shc.switchServer.senderActive');
+        $writeGPIO = RWF::getSetting('shc.switchServer.writeGpio');
+        $readGPIO = RWF::getSetting('shc.switchServer.readGpio');
+
+        //Server initialisieren
+        $this->server = new SocketServer(RWF::getSetting('shc.switchServer.ip'), RWF::getSetting('shc.switchServer.port'));
+        $this->server->startServer();
+
+        //Startmeldung
+        $this->response->writeLnColored(RWF::getLanguage()->get('switchServer.startedSuccessfully', RWF::getSetting('shc.switchServer.ip'), RWF::getSetting('shc.switchServer.port')), 'green');
+
+        //GPIO fuer die Status LED initalisieren
+        if ($sendGpio >= 0) {
+
+            @shell_exec($gpioPath . ' mode ' . $sendGpio . ' out');
+        }
+
+        //auf Anfragen warten
+        while (true) {
+
+            //Anfragen vom Server holen
+            $client = $this->server->accept();
+            $rawData = base64_decode($client->read(8192));
+            $requests = json_decode($rawData, true);
+
+            //Status LED ein
+            if ($sendGpio >= 0) {
+
+                @shell_exec($gpioPath . ' write ' . $sendGpio . ' 1');
+            }
+
+            //Anfragen bearbeiten
+            foreach ($requests as $request) {
+
+                if ($senderActive && isset($request['type']) && $request['type'] = 'radiosocket') {
+
+                    //Funksteckdosen schalten
+                    $this->send433MHzCommand($request);
+                } elseif ($writeGPIO && isset($request['type']) && $request['type'] = 'gpiooutput') {
+
+                    //GPIO schreiben
+                    $this->writeGpio($request);
+                } elseif ($readGPIO && isset($request['type']) && $request['type'] = 'gpioinput') {
+
+                    //GPIO lesen
+                    $this->readGpio($request);
+                } elseif (isset($request['stop']) && $request['stop'] == 1) {
+
+                    //Server Stoppen
+                    $client->close();
+                    $this->stop();
+
+                    //Status LED aus
+                    if ($sendGpio >= 0) {
+
+                        @shell_exec($gpioPath . ' write ' . $sendGpio . ' 0');
+                    }
+                    
+                    return;
+                }
+            }
+
+            //Status LED aus
+            if ($sendGpio >= 0) {
+
+                @shell_exec($gpioPath . ' write ' . $sendGpio . ' 0');
+            }
+
+            //bearbeitung abgeschlossen
+            $client->close();
+        }
+    }
+
+    /**
+     * sendet einen 433MHz Befehl
+     * 
+     * @param Array   $request Anfrage
+     */
+    protected function send433MHzCommand(array $request) {
+
+        @shell_exec('sudo ' . $sendPath . ' ' . $request['protocol'] . ' ' . $request['systemCode'] . ' ' . $request['deviceCode'] . ' ' . $request['command']);
+
+        //Debug ausgabe
+        if ($this->debug) {
+
+            $this->response->writeLnColored('sudo ' . $sendPath . ' ' . $request['protocol'] . ' ' . $request['systemCode'] . ' ' . $request['deviceCode'] . ' ' . $request['command'], 'light_blue');
+        }
+    }
+
+    /**
+     * schreibt einen GPIO
+     * 
+     * @param Array   $request Anfrage
+     */
+    protected function writeGpio(array $request) {
+
+        //Modus setzen
+        if (!in_array($gpio['pinNumber'], $this->gpioModeSet)) {
+
+            @shell_exec($gpioPath . ' mode ' . $request['pinNumber'] . ' out');
+            if ($this->debug) {
+
+                $this->response->writeLnColored($gpioPath . ' mode ' . $request['pinNumber'] . ' out', 'light_green');
+            }
+            $this->gpioModeSet[] = $gpio['pinNumber'];
+        }
+
+        //Befehl ausfuehren
+        @shell_exec($gpioPath . ' write ' . $request['pinNumber'] . ' ' . $request['command']);
+        if ($this->debug) {
+
+            $this->response->writeLnColored($gpioPath . ' write ' . $request['pinNumber'] . ' ' . $request['command'], 'light_green');
+        }
+    }
+
+    /**
+     * liest eine GPIO und sendet die Antwort an den Client
+     * 
+     * @param RWF\IO\SocketServerClient $client  Client Objekt
+     * @param Array                     $request Anfrage
+     */
+    protected function readGpio(SocketServerClient $client, array $request) {
+        
+    }
+
+    /**
+     * beendet den Server
+     */
+    protected function stop() {
+        
+        $this->server->stopServer();
+        $response->writeLnColored(RWF::getLanguage()->get('switchServer.stoppedSuccessfully'), 'green');
+    }
+
+}
