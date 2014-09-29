c065c46644ce661694ffcd06d9d3dc22e094d033
Oliver <Oliver@rpi-controlcenter.de>
2014-09-29 05:09:59 +0200

    Methodenaufrufe Optimiert

--- shc/data/commands/cli/arduinosensorreciverdeamoncli.class.php
+++ shc/data/commands/cli/arduinosensorreciverdeamoncli.class.php
@@ -40,6 +40,57 @@ class ArduinoSensorReciverDeamonCli extends CliCommand {
     protected $debug = false;
 
     /**
+     * fuehrt das Kommando aus
+     */
+    public function executeCommand() {
+
+        global $argv;
+
+        //max_execution_time auser kraft setzen
+        set_time_limit(0);
+        //direkte ausgabe der Daten
+        ob_implicit_flush();
+
+        //Sprache einbinden
+        SHC::getLanguage()->loadModul('ArduinoReciver');
+
+        //pruefen on Server aktiviert
+        if (!SHC::getSetting('shc.arduinoReciver.active')) {
+
+            throw new Exception('Der Arduino Reciver wurde deaktiviert', 1600);
+        }
+
+        //Debug aktivieren
+        if (in_array('-d', $argv) || in_array('--debug', $argv)) {
+
+            $this->debug = true;
+        }
+
+        //Hilfe anzeigen
+        if (in_array('-h', $argv) || in_array('--help', $argv)) {
+
+            $this->writeHelp();
+            return;
+        }
+
+        //Konfiguration
+        if (in_array('-c', $argv) || in_array('--config', $argv)) {
+
+            $this->config();
+            return;
+        }
+
+        //Server Deamon stoppen
+        if (in_array('-s', $argv) || in_array('--stop', $argv)) {
+
+            $this->stop();
+            return;
+        }
+
+        $this->executeCliCommand();
+    }
+    
+    /**
      * gibt die Hilfe zu der Kommandozeilen Funktion auf die Kommandozeile aus
      */
     public function writeHelp() {
@@ -57,23 +108,6 @@ class ArduinoSensorReciverDeamonCli extends CliCommand {
      * fuehrt das CLI Kommando aus
      */
     protected function executeCliCommand() {
-
-        global $argv;
-        
-        //Debug aktivieren
-        if (in_array('-d', $argv) || in_array('--debug', $argv)) {
-
-            $this->debug = true;
-        }
-        
-        //Sprache einbinden
-        SHC::getLanguage()->loadModul('ArduinoReciver');
-
-        //pruefen on Server aktiviert
-        if (!SHC::getSetting('shc.arduinoReciver.active')) {
-
-            throw new Exception('Der Arduino Reciver wurde deaktiviert', 1600);
-        }
         
         $arduinoReciver = new ArduinoSensorReciver(SHC::getSetting('shc.arduinoReciver.interface'), SHC::getSetting('shc.arduinoReciver.baudRate'));
         $arduinoReciver->readDataEndless($this->debug);
