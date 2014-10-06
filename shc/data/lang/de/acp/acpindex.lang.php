aa226821a78f8c943847cdb1d062f6f28ba7b83a
Oliver <Oliver@rpi-controlcenter.de>
2014-10-06 05:43:00 +0200

    ACP Menü implementiert

--- shc/data/lang/de/acp/acpindex.lang.php
+++ shc/data/lang/de/acp/acpindex.lang.php
@@ -19,3 +19,23 @@ $l['acpindex.box.overview'] = 'Übersicht';
 $l['acpindex.box.acpMenue'] = 'Andminsitrations Menü';
 
 //Buttons Hauptmenue
+$l['acpindex.menu.group.main'] = 'Allgemein';
+$l['acpindex.menu.users'] = 'Benutzer';
+$l['acpindex.menu.settings'] = 'Einstellungen';
+$l['acpindex.menu.backup'] = 'Backup';
+
+$l['acpindex.menu.group.smartHome'] = 'Smart Home';
+$l['acpindex.menu.rooms'] = 'Räume';
+$l['acpindex.menu.switchFunctions'] = 'Schaltfunktionen';
+$l['acpindex.menu.sensorPoints'] = 'Sensorpunkte';
+$l['acpindex.menu.userAtHome'] = 'Benutzer zu Hause';
+
+$l['acpindex.menu.group.homeAutomation'] = 'Haus Automatisierung';
+$l['acpindex.menu.conditions'] = 'Bedingungen';
+$l['acpindex.menu.switchPoints'] = 'Schaltpunkte';
+$l['acpindex.menu.events'] = 'Ereignisse';
+
+$l['acpindex.menu.group.other'] = 'Sonstiges';
+$l['acpindex.menu.serverStatus'] = 'Serverstatus';
+$l['acpindex.menu.switchServer'] = 'Schaltserver';
+$l['acpindex.menu.info'] = 'info';
