99285453beecf507b6586ad7c79f92c58d20ce9a
Oliver <Oliver@rpi-controlcenter.de>
2014-12-20 13:29:29 +0100

    Add Grundgerüst für das PCC

---
+++ pcc/data/lang/de/index.lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Startseite Sprachvariablen
+ * 
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+$l = array();
+
+$l['index.button.acpMenu'] = 'Administration';
+$l['index.button.ucp'] = 'Benutzer Bereich';
+$l['index.button.login'] = 'anmelden';
+$l['index.button.logout'] = 'abmelden';
+$l['index.button.back'] = 'zurück';
+$l['index.login.user'] = 'angemeldet als {1:s}';
+$l['index.specialFunctions'] = 'Sonderfunktionen';
+
+//Login
+$l['index.login.boxTitle'] = 'Anmelden';
+$l['index.login.name'] = 'Benutzername';
+$l['index.login.password'] = 'Passwort';
+$l['index.login.longTime'] = 'merken?';
+$l['index.login.submit'] = 'anmelden';
+$l['index.login.requestError'] = 'Anfrage Fehlgeschlagen';
+$l['index.login.error'] = 'Benutzername oder Passwort falsch';
