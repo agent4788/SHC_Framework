b0916dd0a17d4bfed653b2784497bb320aea1e1f
Oliver <Oliver@rpi-controlcenter.de>
2014-12-24 08:15:31 +0100

    Optimierung SHC install

--- shc_install.php
+++ shc_install.php
@@ -1,20 +1,84 @@
 <?php
 
-//SHC Einstellungen
-$settingsXml = new SimpleXMLElement('./rwf/data/storage/default/defaultSettings.xml', null, true);
+/**
+ * Install SHC
+ *
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.3-0
+ * @version    2.0.3-0
+ */
 
-//Einstellung hinzufuegen
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Hilfsfunktionen //////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 define('TYPE_BOOLEAN', 'bool');
 define('TYPE_STRING', 'string');
 define('TYPE_INTEGER', 'int');
+
 function addSetting($name, $value, $type) {
 
     global $settingsXml;
+
+    //pruefen ob Einstellung schon vorhanden
+    foreach($settingsXml->setting as $setting) {
+
+        $attr = $setting->attributes();
+        if($attr->name == $name) {
+
+            return;
+        }
+    }
 
     $setting = $settingsXml->addChild('setting');
     $setting->addAttribute('name', $name);
     $setting->addAttribute('value', $value);
     $setting->addAttribute('type', $type);
+}
+
+function addPremission($xml, $name, $value) {
+
+    //pruefen ob Recht schon vorhanden
+    foreach($xml->premission as $premission) {
+
+        $attr = $premission->attributes();
+        if($attr->name == $name) {
+
+            return;
+        }
+    }
+    $premission = $xml->addChild('premission');
+    $premission->addAttribute('name', $name);
+    $premission->addAttribute('value', $value);
+}
+
+function randomStr($length = 10) {
+
+    $set = array("a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J",
+        "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T",
+        "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "1", "2", "3", "4", "5", "6", "7", "8",
+        "9");
+    $str = '';
+
+    for ($i = 1; $i <= $length; ++$i) {
+
+        $ch = mt_rand(0, count($set) - 1);
+        $str .= $set[$ch];
+    }
+
+    return $str;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Einstellungen ///////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if(!file_exists('./rwf/data/storage/settings.xml')) {
+
+    $settingsXml = new SimpleXMLElement('./rwf/data/storage/default/defaultSettings.xml', null, true);
+} else {
+
+    $settingsXml = new SimpleXMLElement('./rwf/data/storage/settings.xml', null, true);
 }
 
 //Allgemeine Einstellungen
@@ -23,6 +87,9 @@ addSetting('shc.ui.redirectPcTo', '1', TYPE_INTEGER);
 addSetting('shc.ui.redirectTabletTo', '1', TYPE_INTEGER);
 addSetting('shc.ui.redirectSmartphoneTo', '1', TYPE_INTEGER);
 addSetting('shc.ui.index.showUsersAtHome', 'true', TYPE_BOOLEAN);
+addSetting('shc.title', 'SHC%202.0', TYPE_STRING);
+addSetting('shc.defaultStyle', 'redmond', TYPE_STRING);
+addSetting('shc.defaultMobileStyle', 'default', TYPE_STRING);
 
 //Sheduler Daemon
 addSetting('shc.shedulerDaemon.active', 'true', TYPE_BOOLEAN);
@@ -57,40 +124,20 @@ addSetting('shc.sensorTransmitter.ip', '127.0.0.1', TYPE_STRING);
 addSetting('shc.sensorTransmitter.port', '9275', TYPE_STRING);
 addSetting('shc.sensorTransmitter.pointId', '-1', TYPE_INTEGER);
 
-//Allgemeine SHC Einstellungen
-addSetting('shc.title', 'SHC%202.0', TYPE_STRING);
-addSetting('shc.defaultStyle', 'redmond', TYPE_STRING);
-addSetting('shc.defaultMobileStyle', 'default', TYPE_STRING);
-
 //XML Speichern
 $settingsXml->asXML('./rwf/data/storage/settings.xml');
 
-//Benutzerverwaltung Konfigurieren
-$usersXml = new SimpleXMLElement('./rwf/data/storage/default/defaultUsers.xml', null, true);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Berechtigungen //////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$newXml = false;
+if(!file_exists('./rwf/data/storage/users.xml')) {
 
-//Funktionen
-function addPremission($xml, $name, $value) {
+    $usersXml = new SimpleXMLElement('./rwf/data/storage/default/defaultUsers.xml', null, true);
+    $newXml = true;
+} else {
 
-    $premission = $xml->addChild('premission');
-    $premission->addAttribute('name', $name);
-    $premission->addAttribute('value', $value);
-}
-
-function randomStr($length = 10) {
-
-    $set = array("a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J",
-        "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T",
-        "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "1", "2", "3", "4", "5", "6", "7", "8",
-        "9");
-    $str = '';
-
-    for ($i = 1; $i <= $length; ++$i) {
-
-        $ch = mt_rand(0, count($set) - 1);
-        $str .= $set[$ch];
-    }
-
-    return $str;
+    $usersXml = new SimpleXMLElement('./rwf/data/storage/users.xml', null, true);
 }
 
 //Gruppenrechte vorbereiten
@@ -114,15 +161,29 @@ foreach($usersXml->groups->group as $group) {
     addPremission($group, 'shc.acp.switchserverManagement', '0');
 }
 
-//Passwort Libary einbinden fuer PHP Versionen < PHP 5.5
-require_once('./rwf/lib/external/password/password.php');
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Benutzer ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if($newXml === true) {
 
-//Hauptbenutzer vorbereiten
-$user = $usersXml->users->user[0];
-$user->name = 'admin';
-$user->password = password_hash('admin', PASSWORD_DEFAULT);
-$user->authCode = randomStr(64);
-$user->register = (new DateTime('now'))->format('Y-m-d');
+    //Passwort Libary einbinden fuer PHP Versionen < PHP 5.5
+    require_once('./rwf/lib/external/password/password.php');
+
+    //Hauptbenutzer vorbereiten
+    $user = $usersXml->users->user[0];
+    $user->name = 'admin';
+    $user->password = password_hash('admin', PASSWORD_DEFAULT);
+    $user->authCode = randomStr(64);
+    $user->register = (new DateTime('now'))->format('Y-m-d');
+}
 
 //XML Speichern
 $usersXml->asXML('./rwf/data/storage/users.xml');
+
+print("SHC erfolgreich installiert\n");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// sich selbst loeschen ////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@unlink(__FILE__);
