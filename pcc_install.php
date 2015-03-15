29ecc40b388782e6007dcf049e88c0d13539f397
Oliver <Oliver@rpi-controlcenter.de>
2015-03-15 06:33:26 +0100

    PCC Reboot/Shutdown Buttons in Kopfzeile entfernt

--- pcc_install.php
+++ pcc_install.php
@@ -113,8 +113,6 @@ foreach($usersXml->groups->group as $group) {
     $group = $group->premissions;
     addPremission($group, 'pcc.ucp.viewSysState', '0');
     addPremission($group, 'pcc.ucp.viewSysData', '0');
-    addPremission($group, 'pcc.ucp.reboot', '0');
-    addPremission($group, 'pcc.ucp.shutdown', '0');
     addPremission($group, 'pcc.acp.menu', '0');
     addPremission($group, 'pcc.acp.userManagement', '0');
     addPremission($group, 'pcc.acp.settings', '0');
