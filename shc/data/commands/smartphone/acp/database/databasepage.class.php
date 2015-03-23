526bc1106fca33bc7386d24c4a05b75ddb2f85e5
Oliver <Oliver@rpi-controlcenter.de>
2015-03-23 06:27:30 +0100

    Add SHC mobile Datenbankverwaltung Issue #15

---
+++ shc/data/commands/smartphone/acp/database/databasepage.class.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace SHC\Command\Smartphone;
+
+//Imports
+use RWF\Date\DateTime;
+use RWF\Request\Commands\PageCommand;
+use RWF\Request\Request;
+use RWF\Util\Message;
+use SHC\Core\SHC;
+
+/**
+ * Zeigt eine Liste mit allen Benutzern an
+ *
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+class DatabasePage extends PageCommand {
+
+    protected $template = 'database.html';
+
+    protected $premission = 'shc.acp.databaseManagement';
+
+    /**
+     * Sprachpakete die geladen werden sollen
+     *
+     * @var Array
+     */
+    protected $languageModules = array('acpindex', 'database', 'index');
+
+    /**
+     * Daten verarbeiten
+     */
+    public function processData() {
+
+        $tpl = SHC::getTemplate();
+        $db = SHC::getDatabase();
+
+        //Headline Daten
+        $tpl->assign('apps', SHC::listApps());
+        $tpl->assign('acp', true);
+        $tpl->assign('style', SHC::getStyle());
+        $tpl->assign('user', SHC::getVisitor());
+        $tpl->assign('backLink', 'index.php?app=shc&m&page=acp');
+        $tpl->assign('device', SHC_DETECTED_DEVICE);
+        if(SHC::getSession()->getMessage() != null) {
+            $tpl->assign('message', SHC::getSession()->getMessage());
+            SHC::getSession()->removeMessage();
+        }
+
+        //Datenbank Dump
+        if($this->request->issetParam('dump', Request::GET)) {
+
+            $message = new Message();
+            if($db->bgsave()) {
+
+                $message->setType(Message::SUCCESSFULLY);
+                $message->setMessage(SHC::getLanguage()->get('acp.database.dump.success'));
+            } else {
+
+                $message->setType(Message::ERROR);
+                $message->setMessage(SHC::getLanguage()->get('acp.database.dump.error'));
+            }
+            $tpl->assign('message', $message);
+        }
+
+        //Daten
+        $info = $db->info();
+        if(isset($info['rdb_last_save_time'])){
+
+            $lastSave = $info['rdb_last_save_time'];
+        } elseif($info['last_save_time']) {
+
+            $lastSave = $info['last_save_time'];
+        } else {
+
+            $lastSave = 0;
+        }
+        $lastSaveDate = DateTime::now();
+        $lastSaveDate->setTimestamp($lastSave);
+
+        $tpl->assign('info', $info);
+        $tpl->assign('lastSaveDate', $lastSaveDate);
+    }
+
+}
