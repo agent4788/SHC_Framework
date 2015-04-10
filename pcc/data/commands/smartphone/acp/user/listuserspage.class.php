7ca7b3969c98a902223fb3d5a1726a5780ecc7aa
Oliver <Oliver@rpi-controlcenter.de>
2015-04-10 06:35:36 +0200

    Add PCC mobile Benutzerverwaltung Issue #17

---
+++ pcc/data/commands/smartphone/acp/user/listuserspage.class.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PCC\Command\Smartphone;
+
+//Imports
+use PCC\Core\PCC;
+use RWF\Core\RWF;
+use RWF\Request\Commands\PageCommand;
+use RWF\User\UserEditor;
+
+/**
+ * Zeigt eine Liste mit allen Benutzern an
+ *
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+class ListUsersPage extends PageCommand {
+
+    protected $template = 'userlist.html';
+
+    protected $premission = 'pcc.acp.userManagement';
+
+    /**
+     * Sprachpakete die geladen werden sollen
+     *
+     * @var Array
+     */
+    protected $languageModules = array('index', 'usermanagement', 'acpindex');
+
+    /**
+     * Daten verarbeiten
+     */
+    public function processData() {
+
+        $tpl = RWF::getTemplate();
+
+        //Headline Daten
+        $tpl->assign('apps', PCC::listApps());
+        $tpl->assign('acp', true);
+        $tpl->assign('style', PCC::getStyle());
+        $tpl->assign('user', PCC::getVisitor());
+        $tpl->assign('backLink', 'index.php?app=pcc&m&page=acp');
+        $tpl->assign('device', PCC_DETECTED_DEVICE);
+        if(RWF::getSession()->getMessage() != null) {
+            $tpl->assign('message', RWF::getSession()->getMessage());
+            RWF::getSession()->removeMessage();
+        }
+
+        //Benutzer Liste
+        $tpl->assign('userList', UserEditor::getInstance()->listUsers(UserEditor::SORT_BY_NAME));
+    }
+
+}
