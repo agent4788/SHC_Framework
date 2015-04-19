8830b3e223141b11a20bfca1164c6740b0bfa839
Oliver <Oliver@rpi-controlcenter.de>
2015-04-19 05:30:32 +0200

    SHC Template Plugins angepasst Issue #67

--- shc/lib/template/plugin/editlistcompilerplugin.class.php
+++ shc/lib/template/plugin/editlistcompilerplugin.class.php
@@ -51,9 +51,7 @@ class EditListCompilerPlugin implements TemplateCompilerPlugin {
         $html .= '      }';
         $html .= '  });';
         $html .= '  $(\'#<?php echo $randomId; ?>\').click(function() {';
-        $html .= '      $.get(\''. $link .'\', function(data, textStatus, jqXHR) {;';
-        $html .= '          $(\'#shc-view-acp-contentBox div.shc-contentbox-inner\').html(data);';
-        $html .= '      });';
+        $html .= '      window.location = \''. $link .'\';';
         $html .= '  });';
         $html .= '});';
         $html .= '</script>';
