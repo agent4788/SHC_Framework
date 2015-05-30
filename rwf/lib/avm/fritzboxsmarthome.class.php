3aa10d7e174ab4e3eb90981abb5a802b80674782
Oliver <webmaster@rpi-controlcenter.de>
2015-05-30 16:44:43 +0200

    Add RWF FritzBox SOAP API

---
+++ rwf/lib/avm/fritzboxsmarthome.class.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace RWF\AVM;
+
+//Imports
+use RWF\Core\RWF;
+use RWF\Util\ArrayUtil;
+use RWF\XML\XmlEditor;
+
+
+/**
+ * Auslesen der Fritz Box SmartHome Geraete
+ *
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2015, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+class FritzBoxSmartHome extends FritzBox {
+
+    /**
+     * @param string $address      Fritz Box Host oder IP Adresse
+     * @param bool   $has5GhzWlan  Fritz Box Host oder IP Adresse
+     * @param string $user         Benutzername (Fritz Box Benutzer)
+     * @param string $password     Passwort (Fritz Box Benutzer)
+     */
+    public function __construct($address = 'fritz.box', $has5GhzWlan = true, $user = '', $password = '') {
+
+        parent::__construct($address, $has5GhzWlan, $user, $password);
+    }
+
+    /**
+     * gibt die Geraeteliste zurueck
+     *
+     * @return array
+     */
+    public function getDeviceList() {
+
+        if(!isset($this->cache['smarthome']['deviceList'])) {
+
+            //HTTP Anfrage
+            $http_options = stream_context_create(array(
+                'http' => array(
+                    'method'  => 'GET',
+                    'user_agent' => "RWF Framework Version ". RWF::VERSION
+                )
+            ));
+            $result = @file_get_contents('http://'. $this->address .'/webservices/homeautoswitch.lua?switchcmd=getswitchlist&'. $this->getSid(), false, $http_options);
+            $list = explode(',', $result);
+            $list = ArrayUtil::trim($list);
+            $this->cache['smarthome']['deviceList'] = $list;
+        }
+        return $this->cache['smarthome']['deviceList'];
+    }
+
+    /**
+     * gibt eine liste mit allen SmartHome Geraeten zurueck
+     *
+     * @return array
+     * @throws \RWF\XML\Exception\XmlException
+     */
+    public function listDevices() {
+
+        if(!isset($this->cache['smarthome']['devices'])) {
+
+            $list = $this->getDeviceList();
+            foreach($list as $device) {
+
+                $data = $this->getDevice($device);
+                $this->cache['smarthome']['devices'][] = $data;
+            }
+        }
+        return $this->cache['smarthome']['devices'];
+    }
+
+    /**
+     * gibt die Daten des Geraetes mit der Ain zurueck
+     *
+     * @param  string $ain Geraete Identifikation
+     * @return array
+     */
+    public function getDevice($ain) {
+
+        //HTTP Anfrage
+        $http_options = stream_context_create(array(
+            'http' => array(
+                'method'  => 'GET',
+                'user_agent' => "RWF Framework Version ". RWF::VERSION
+            )
+        ));
+        $result = @file_get_contents('http://'. $this->address .'/webservices/homeautoswitch.lua?switchcmd=getdevicelistinfos&ain='. urlencode($ain) .'&'. $this->getSid(), false, $http_options);
+
+        //XML Daten auswerten
+        $xml = XmlEditor::createFromString($result);
+        $atrr = $xml->device->attributes();
+        $data = array(
+            'device' => array(
+                'ain' => $ain,
+                'identifier' => (string) $atrr->identifier,
+                'id' => (string) $atrr->id,
+                'functionbitmask' => (string) $atrr->functionbitmask,
+                'fwversion' => (string) $atrr->fwversion,
+                'manufacturer' => (string) $atrr->manufacturer,
+                'productname' => (string) $atrr->productname,
+            ),
+
+            'present' => ((int) $xml->device->present == 1 ? true : false),
+            'name' => (string) $xml->device->name,
+            'switch' => array(
+                'state' => (string) $xml->device->switch->state,
+                'mode' => (string) $xml->device->switch->mode,
+                'lock' => ((int) $xml->device->switch->lock == 1 ? true : false),
+            ),
+            'powermeter' => array(
+                'power' => (int) $xml->device->powermeter->power,
+                'energy' => (int) $xml->device->powermeter->energy,
+            ),
+            'temperature' => array(
+                'temperature' => ((float) $xml->device->temperature->celsius != 0.0 ? (float) $xml->device->temperature->celsius / 10 : 0.0),
+                'offset' => ((float) $xml->device->temperature->offset != 0.0 ? (float) $xml->device->temperature->offset / 10 : 0.0)
+            )
+        );
+        return $data;
+    }
+
+    /**
+     * schaltet die Steckdose an
+     *
+     * @param  string $ain Geraete Identifikation
+     * @return bool
+     */
+    public function switchOn($ain) {
+
+        //HTTP Anfrage
+        $http_options = stream_context_create(array(
+            'http' => array(
+                'method'  => 'GET',
+                'user_agent' => "RWF Framework Version ". RWF::VERSION
+            )
+        ));
+        $result = @file_get_contents('http://'. $this->address .'/webservices/homeautoswitch.lua?switchcmd=setswitchon&ain='. urlencode($ain) .'&'. $this->getSid(), false, $http_options);
+
+        //erfolg pruefen
+        if($result !== false) {
+
+            if(trim($result) == 1) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * schaltet die Steckdose aus
+     *
+     * @param  string $ain Geraete Identifikation
+     * @return bool
+     */
+    public function switchOff($ain) {
+
+        //HTTP Anfrage
+        $http_options = stream_context_create(array(
+            'http' => array(
+                'method'  => 'GET',
+                'user_agent' => "RWF Framework Version ". RWF::VERSION
+            )
+        ));
+        $result = @file_get_contents('http://'. $this->address .'/webservices/homeautoswitch.lua?switchcmd=setswitchoff&ain='. urlencode($ain) .'&'. $this->getSid(), false, $http_options);
+
+        //erfolg pruefen
+        if($result !== false) {
+
+            if(trim($result) == 0) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * schaltet die Steckdose um
+     *
+     * @param  string $ain Geraete Identifikation
+     * @return bool
+     */
+    public function switchToggle($ain) {
+
+        //HTTP Anfrage
+        $http_options = stream_context_create(array(
+            'http' => array(
+                'method'  => 'GET',
+                'user_agent' => "RWF Framework Version ". RWF::VERSION
+            )
+        ));
+        $result = @file_get_contents('http://'. $this->address .'/webservices/homeautoswitch.lua?switchcmd=setswitchtoggle&ain='. urlencode($ain) .'&'. $this->getSid(), false, $http_options);
+
+        //erfolg pruefen
+        if($result !== false) {
+
+            if(trim($result) == 0) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * der Cache wird geloescht und neu erzeugt
+     *
+     * @return bool
+     */
+    public function rebuildCache() {
+
+        parent::rebuildCache();
+        $this->cache['smarthome'] = array();
+    }
+}
---
+++ rwf/lib/avm/fritzboxwlan.class.php
@@ -0,0 +1,499 @@
+<?php
+
+namespace RWF\AVM;
+
+//Imports
+
+
+/**
+ * Auslesen der Fritz Box Wlandaten
+ *
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2015, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+class FritzBoxWlan extends FritzBox {
+
+    /**
+     * gibt den Status des ersten WLANs zurueck
+     * das ist immer das 2,4GHz WLAN
+     *
+     * @return array
+     * @throws \SoapFault
+     */
+    protected function getWlan1State() {
+
+        if(!isset($this->cache['wlan']['wlan1'])) {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig1",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:1",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $this->cache['wlan']['wlan1'] = $client->GetInfo();
+        }
+        return $this->cache['wlan']['wlan1'];
+    }
+
+    /**
+     * gibt den Status des zweiten WLANs zurueck
+     * das ist wenn der Router ein 5GHz WLAN hat das 5GHz WLAN und wenn nicht das Gäste WLAN
+     *
+     * @return array
+     * @throws \SoapFault
+     */
+    protected function getWlan2State() {
+
+        if(!isset($this->cache['wlan']['wlan2'])) {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig2",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:2",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $this->cache['wlan']['wlan2'] = $client->GetInfo();
+        }
+        return $this->cache['wlan']['wlan2'];
+    }
+
+    /**
+     * gibt den Status des dritten WLANs zurueck
+     * das ist immer das Gast WLAN (wenn vorhanden)
+     *
+     * @return array
+     * @throws \SoapFault
+     */
+    protected function getWlan3State() {
+
+        if(!isset($this->cache['wlan']['wlan3'])) {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig3",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:3",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $this->cache['wlan']['wlan3'] = $client->GetInfo();
+        }
+        return $this->cache['wlan']['wlan3'];
+    }
+
+    /**
+     * gibt an ob das WLAN aktiv ist
+     *
+     * @return bool
+     */
+    public function isGuestWlanEnabled() {
+
+        if($this->has5GhzWlan == true) {
+
+            $info = $this->getWlan3State();
+        } else {
+
+            $info = $this->getWlan2State();
+        }
+        return ((int) $info['NewEnable'] == 1 ? true : false);
+    }
+
+    /**
+     * gibt die MAC Adresse des WLANs zurueck
+     *
+     * @return string
+     */
+    public function getGuestWlanMacAddress() {
+
+        if($this->has5GhzWlan == true) {
+
+            $info = $this->getWlan3State();
+        } else {
+
+            $info = $this->getWlan2State();
+        }
+        return (isset($info['NewBSSID']) ? $info['NewBSSID'] : null);
+    }
+
+    /**
+     * gibt die SSID des WLANs zurueck
+     *
+     * @return string
+     */
+    public function getGuestWlanSSID() {
+
+        if($this->has5GhzWlan == true) {
+
+            $info = $this->getWlan3State();
+        } else {
+
+            $info = $this->getWlan2State();
+        }
+        return (isset($info['NewSSID']) ? $info['NewSSID'] : null);
+    }
+
+    /**
+     * gibt an ob fie MAC Adress Kontrolle fuer das WLAN aktiv ist
+     *
+     * @return bool
+     */
+    public function isGuestWlanMacAddressControlEnabled() {
+
+        if($this->has5GhzWlan == true) {
+
+            $info = $this->getWlan3State();
+        } else {
+
+            $info = $this->getWlan2State();
+        }
+        return ((int) $info['NewMACAddressControlEnabled'] == 1 ? true : false);
+    }
+
+    /**
+     * gibt den verwendetetn WLAN Standard zurueck
+     *
+     * @return string
+     */
+    public function getGuestWlanStandard() {
+
+        if($this->has5GhzWlan == true) {
+
+            $info = $this->getWlan3State();
+        } else {
+
+            $info = $this->getWlan2State();
+        }
+        return (isset($info['NewStandard']) ? $info['NewStandard'] : null);
+    }
+
+    /**
+     * gibt die Maximale Bitrate des WLANs zurueck
+     *
+     * @return string
+     */
+    public function getGuestWlanMaxBitRate() {
+
+        if($this->has5GhzWlan == true) {
+
+            $info = $this->getWlan3State();
+        } else {
+
+            $info = $this->getWlan2State();
+        }
+        return (isset($info['NewMaxBitRate']) ? $info['NewMaxBitRate'] : null);
+    }
+
+    /**
+     * aktiviert das Gast WLAN
+     *
+     * @throws \SoapFault
+     */
+    public function enableGuestWlan() {
+
+        if($this->has5GhzWlan == true) {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig3",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:3",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $client->SetEnable(new \SoapParam(1,'NewEnable'));
+        } else {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig2",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:2",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $client->SetEnable(new \SoapParam(1,'NewEnable'));
+        }
+    }
+
+    /**
+     * deaktiviert das Gast WLAN
+     *
+     * @throws \SoapFault
+     */
+    public function disableGuestWlan() {
+
+        if($this->has5GhzWlan == true) {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location' => "http://". $this->address .":49000/upnp/control/wlanconfig3",
+                    'uri' => "urn:dslforum-org:service:WLANConfiguration:3",
+                    'noroot' => True,
+                    'login' => $this->user,
+                    'password' => $this->password
+                )
+            );
+            $client->SetEnable(new \SoapParam(0, 'NewEnable'));
+        } else {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig2",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:2",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $client->SetEnable(new \SoapParam(0,'NewEnable'));
+        }
+    }
+
+    /**
+     * gibt an ob das WLAN aktiv ist
+     *
+     * @return bool
+     */
+    public function is2GHzWlanEnabled() {
+
+        $info = $this->getWlan1State();
+        return ((int) $info['NewEnable'] == 1 ? true : false);
+    }
+
+    /**
+     * gibt die MAC Adresse des WLANs zurueck
+     *
+     * @return string
+     */
+    public function get2GHzWlanMacAddress() {
+
+        $info = $this->getWlan1State();
+        return (isset($info['NewBSSID']) ? $info['NewBSSID'] : null);
+    }
+
+    /**
+     * gibt die SSID des WLANs zurueck
+     *
+     * @return string
+     */
+    public function get2GHzWlanSSID() {
+
+        $info = $this->getWlan1State();
+        return (isset($info['NewSSID']) ? $info['NewSSID'] : null);
+    }
+
+    /**
+     * gibt an ob fie MAC Adress Kontrolle fuer das WLAN aktiv ist
+     *
+     * @return bool
+     */
+    public function is2GHzWlanMacAddressControlEnabled() {
+
+        $info = $this->getWlan1State();
+        return ((int) $info['NewMACAddressControlEnabled'] == 1 ? true : false);
+    }
+
+    /**
+     * gibt den verwendetetn WLAN Standard zurueck
+     *
+     * @return string
+     */
+    public function get2GHzWlanStandard() {
+
+        $info = $this->getWlan1State();
+        return (isset($info['NewStandard']) ? $info['NewStandard'] : null);
+    }
+
+    /**
+     * gibt die Maximale Bitrate des WLANs zurueck
+     *
+     * @return string
+     */
+    public function get2GHzWlanMaxBitRate() {
+
+        $info = $this->getWlan1State();
+        return (isset($info['NewMaxBitRate']) ? $info['NewMaxBitRate'] : null);
+    }
+
+    /**
+     * aktiviert das 2,4GHz WLAN
+     *
+     * @throws \SoapFault
+     */
+    public function enable2GHzWlan() {
+
+        $client = new \SoapClient(
+            null,
+            array(
+                'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig1",
+                'uri'        => "urn:dslforum-org:service:WLANConfiguration:1",
+                'noroot'     => True,
+                'login'      => $this->user,
+                'password'   => $this->password
+            )
+        );
+        $client->SetEnable(new \SoapParam(1,'NewEnable'));
+    }
+
+    /**
+     * deaktiviert das 2,4GHz WLAN
+     *
+     * @throws \SoapFault
+     */
+    public function disable2GHzWlan() {
+
+        $client = new \SoapClient(
+            null,
+            array(
+                'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig1",
+                'uri'        => "urn:dslforum-org:service:WLANConfiguration:1",
+                'noroot'     => True,
+                'login'      => $this->user,
+                'password'   => $this->password
+            )
+        );
+        $client->SetEnable(new \SoapParam(0,'NewEnable'));
+    }
+
+    /**
+     * gibt an ob das WLAN aktiv ist
+     *
+     * @return bool
+     */
+    public function is5GHzWlanEnabled() {
+
+        $info = $this->getWlan3State();
+        return ((int) $info['NewEnable'] == 1 ? true : false);
+    }
+
+    /**
+     * gibt die MAC Adresse des WLANs zurueck
+     *
+     * @return string
+     */
+    public function get5GHzWlanMacAddress() {
+
+        $info = $this->getWlan3State();
+        return (isset($info['NewBSSID']) ? $info['NewBSSID'] : null);
+    }
+
+    /**
+     * gibt die SSID des WLANs zurueck
+     *
+     * @return string
+     */
+    public function get5GHzWlanSSID() {
+
+        $info = $this->getWlan3State();
+        return (isset($info['NewSSID']) ? $info['NewSSID'] : null);
+    }
+
+    /**
+     * gibt an ob fie MAC Adress Kontrolle fuer das WLAN aktiv ist
+     *
+     * @return bool
+     */
+    public function is5GHzWlanMacAddressControlEnabled() {
+
+        $info = $this->getWlan3State();
+        return ((int) $info['NewMACAddressControlEnabled'] == 1 ? true : false);
+    }
+
+    /**
+     * gibt den verwendetetn WLAN Standard zurueck
+     *
+     * @return string
+     */
+    public function get5GHzWlanStandard() {
+
+        $info = $this->getWlan3State();
+        return (isset($info['NewStandard']) ? $info['NewStandard'] : null);
+    }
+
+    /**
+     * gibt die Maximale Bitrate des WLANs zurueck
+     *
+     * @return string
+     */
+    public function get5GHzWlanMaxBitRate() {
+
+        $info = $this->getWlan3State();
+        return (isset($info['NewMaxBitRate']) ? $info['NewMaxBitRate'] : null);
+    }
+
+    /**
+     * aktiviert das 5GHz WLAN
+     *
+     * @throws \SoapFault
+     */
+    public function enable5GHzWlan() {
+
+        if($this->has5GhzWlan == true) {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig2",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:2",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $client->SetEnable(new \SoapParam(1,'NewEnable'));
+        }
+    }
+
+    /**
+     * deaktiviert das 5GHz WLAN
+     *
+     * @throws \SoapFault
+     */
+    public function disable5GHzWlan() {
+
+        if($this->has5GhzWlan == true) {
+
+            $client = new \SoapClient(
+                null,
+                array(
+                    'location'   => "http://". $this->address .":49000/upnp/control/wlanconfig2",
+                    'uri'        => "urn:dslforum-org:service:WLANConfiguration:2",
+                    'noroot'     => True,
+                    'login'      => $this->user,
+                    'password'   => $this->password
+                )
+            );
+            $client->SetEnable(new \SoapParam(0,'NewEnable'));
+        }
+    }
+
+    /**
+     * der Cache wird geloescht und neu erzeugt
+     *
+     * @return bool
+     */
+    public function rebuildCache() {
+
+        parent::rebuildCache();
+        $this->cache['wlan'] = array();
+    }
+}
