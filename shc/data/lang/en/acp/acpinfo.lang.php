c98adef605185682dedcaba26b7395f06d0bb293
Dietmar-Franken <d.franken@umsons.de>
2015-06-26 14:53:56 +0200

    Create acpinfo.lang.php

---
+++ shc/data/lang/en/acp/acpinfo.lang.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * ACP Info Sprachvariablen
+ *
+ * @author       Oliver Kleditzsch
+ * @copyright    Copyright (c) 2014, Oliver Kleditzsch
+ * @license      http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since        2.0.0-0
+ * @version      2.0.0-0
+ * @translation  Dietmar Franken
+ * @SHC Forum    http://rpi-controlcenter.de/member.php?action=profile&uid=173
+ */
+ 
+$l = array();
+
+//Titel
+
+//$l['acp.acpinfo.title'] = 'System Informationen';
+$l['acp.acpinfo.title'] = 'System informations';
+
+//SHC Version
+
+$l['acp.acpinfo.box.shcVersion'] = 'SHC Version';
+$l['acp.acpinfo.box.shcVersion.rwf'] = 'RWF Version';
+$l['acp.acpinfo.box.shcVersion.shc'] = 'SHC Version';
+//$l['acp.acpinfo.box.shcVersion.size'] = 'Speicherbedarf';
+$l['acp.acpinfo.box.shcVersion.size'] = 'Memory requirements';
+
+//Versionen
+//Versions
+
+//$l['acp.acpinfo.box.version'] = 'Versionen';
+$l['acp.acpinfo.box.version'] = 'Versions';
+$l['acp.acpinfo.box.version.php'] = 'PHP';
+$l['acp.acpinfo.box.version.wiringPi'] = 'wiringPi';
+//$l['acp.acpinfo.box.version.wiringPi.unknown'] = 'unbekannt';
+$l['acp.acpinfo.box.version.wiringPi.unknown'] = 'unknown';
+$l['acp.acpinfo.box.version.jQuery'] = 'jQuery';
+$l['acp.acpinfo.box.version.jQueryUi'] = 'jQuery Ui';
+$l['acp.acpinfo.box.version.jQueryMobile'] = 'jQuery Mobile';
+$l['acp.acpinfo.box.version.mobileDetect'] = 'Mobile Detect';
+$l['acp.acpinfo.box.version.jQueryCookie'] = 'jQuery Cookie';
+
+//Schreibrechte
+//Write permissions
+
+//$l['acp.acpinfo.box.writePremissions.allowed'] = 'schreibbar';
+$l['acp.acpinfo.box.writePremissions.allowed'] = 'writable';
+//$l['acp.acpinfo.box.writePremissions.denied'] = 'nicht schreibbar';
+$l['acp.acpinfo.box.writePremissions.denied'] = 'not writable';
+//$l['acp.acpinfo.box.writePremissions'] = 'Schreibrechte';
+$l['acp.acpinfo.box.writePremissions'] = 'Write permissions';
+
+//Schreibrechte
+//Write permissions
+
+$l['acp.acpinfo.box.credits'] = 'Credits';
+//$l['acp.acpinfo.box.credits.engineer'] = 'Entwickler';
+$l['acp.acpinfo.box.credits.engineer'] = 'Developer';
+//$l['acp.acpinfo.box.credits.projectPage'] = 'Projektseite';
+$l['acp.acpinfo.box.credits.projectPage'] = 'Project page';
+//$l['acp.acpinfo.box.credits.licence'] = 'Lizenzhinweise';
+$l['acp.acpinfo.box.credits.licence'] = 'License notices';
+//$l['acp.acpinfo.box.credits.page'] = 'Webseite';
+$l['acp.acpinfo.box.credits.page'] = 'Website';
