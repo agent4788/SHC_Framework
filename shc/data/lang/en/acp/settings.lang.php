ddccbeff651973a6af66c2a858f3102221024279
Dietmar-Franken <d.franken@umsons.de>
2015-06-30 04:17:02 +0200

    Update settings.lang.php

--- shc/data/lang/en/acp/settings.lang.php
+++ shc/data/lang/en/acp/settings.lang.php
@@ -1,13 +1,15 @@
 <?php
 
 /**
- * Einstellungen Sprachvariablen
+ * ACP Info Sprachvariablen
  *
- * @author     Oliver Kleditzsch
- * @copyright  Copyright (c) 2014, Oliver Kleditzsch
- * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @since      2.0.0-0
- * @version    2.0.0-0
+ * @author       Oliver Kleditzsch
+ * @copyright    Copyright (c) 2014, Oliver Kleditzsch
+ * @license      http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since        2.0.0-0
+ * @version      2.0.0-0
+ * @translation  Dietmar Franken
+ * @SHC Forum    http://rpi-controlcenter.de/member.php?action=profile&uid=173
  */
  
 $l = array();
@@ -46,95 +48,94 @@ $l['acp.settings.tabs.ui.description'] = 'Einstellungen zur Benutzeroberfläche'
 $l['acp.settings.tabs.fritzBox'] = 'Settings to the user interface';
 //$l['acp.settings.tabs.fritzBox.description'] = 'Zugangsdaten zur Fritz Box';
 $l['acp.settings.tabs.fritzBox.description'] = 'Login information to the Fritz box';
-//
-$l['acp.settings.tabs.redirect.pc'] = 'Weboberfläche';
-//
-$l['acp.settings.tabs.redirect.smartphone'] = 'Mobiloberfläche';
-//
-$l['acp.settings.tabs.timeline.true'] = 'Intervall (vor 5 Minuten)';
-//
-$l['acp.settings.tabs.timeline.false'] = 'Absulout (11.05.2014 11:15:36)';
-//
-$l['acp.settings.form.title'] = 'Titel';
-//
-$l['acp.settings.form.title.decription'] = 'Titel der in der Kopfzeile angezeit wird';
-//
-$l['acp.settings.form.allowLongTimeLogin'] = 'Langzeit Login';
-//
-$l['acp.settings.form.allowLongTimeLogin.decription'] = 'Benutzer können sich dauerhaft einlogge';
-//
+//$l['acp.settings.tabs.redirect.pc'] = 'Weboberfläche';
+$l['acp.settings.tabs.redirect.pc'] = 'Web interface';
+//$l['acp.settings.tabs.redirect.smartphone'] = 'Mobiloberfläche';
+$l['acp.settings.tabs.redirect.smartphone'] = 'Mobile interface';
+//$l['acp.settings.tabs.timeline.true'] = 'Intervall (vor 5 Minuten)';
+$l['acp.settings.tabs.timeline.true'] = 'Interval (5 minutes ago)';
+//$l['acp.settings.tabs.timeline.false'] = 'Absolut (11.05.2014 11:15:36)';
+$l['acp.settings.tabs.timeline.false'] = 'Absolut (05/11/2014 11:15:36)';
+//$l['acp.settings.form.title'] = 'Titel';
+$l['acp.settings.form.title'] = 'Title';
+//$l['acp.settings.form.title.decription'] = 'Titel der in der Kopfzeile angezeigt wird';
+$l['acp.settings.form.title.decription'] = 'Title to be displayed in the header';
+//$l['acp.settings.form.allowLongTimeLogin'] = 'Langzeit Login';
+$l['acp.settings.form.allowLongTimeLogin'] = 'Long time login';
+//$l['acp.settings.form.allowLongTimeLogin.decription'] = 'Benutzer können sich dauerhaft einloggen';
+$l['acp.settings.form.allowLongTimeLogin.decription'] = 'Users can login permanently';
 $l['acp.settings.form.defaultStyle'] = 'Webstyle';
-//
-$l['acp.settings.form.defaultStyle.decription'] = 'der hier eingestellte Webstyle gilt für alle Gäste und Benutzer die nicht selbst einen Style gewählt haben';
-//
-$l['acp.settings.form.defaultLanguage'] = 'Sprache';
-//
-$l['acp.settings.form.defaultLanguage.decription'] = 'die hier eingestellte Sprache gilt für alle Gäste und Benutzer die nicht selbst ein Sprache gewählt haben';
-//
-$l['acp.settings.form.Timezone'] = 'Zeitzone';
-//
-$l['acp.settings.form.Timezone.decription'] = 'Zeitzone';
-//
-$l['acp.settings.form.defaultDateFormat'] = 'Datumsformat';
-//
-$l['acp.settings.form.defaultDateFormat.decription'] = 'Standard Datumsformat, die Bedutung der Formatierung ist in der PHP Funktionsbeschreibung der "date" Funktion zu finden';
-//
-$l['acp.settings.form.defaultTimeFormat'] = 'Zeitformat';
-//
-$l['acp.settings.form.defaultTimeFormat.decription'] = 'Standard Zeitformat, die Bedutung der Formatierung ist in der PHP Funktionsbeschreibung der "date" Funktion zu finden';
-//
-$l['acp.settings.form.useTimeline'] = 'Zeitanzeige';
-//
-$l['acp.settings.form.useTimeline.decription'] = 'legt fest wie Datum un Urzeit angezeigt werden sollen';
-//
-$l['acp.settings.form.sunriseOffset'] = 'Offset Sonnenaufgang';
-//
-$l['acp.settings.form.sunriseOffset.decription'] = 'mit dieser Einstellung kann der errechnete Sonnenaufgang verschoben werden (heutiger Sonnenaufgang mit Offset: {1:s} Uhr)';
-//
-$l['acp.settings.form.sunsetOffset'] = 'Offset Sonnenuntergang';
-//
-$l['acp.settings.form.sunsetOffset.decription'] = 'mit dieser Einstellung kann der errechnete Sonnenuntergang verschoben werden (heutiger Sonnenuntergang mit Offset: {1:s} Uhr)';
-//
-$l['acp.settings.form.Latitude'] = 'Breitengrad';
-//
-$l['acp.settings.form.Latitude.decription'] = 'Geodaten für die Berechnung von Sonnenauf- und untergang';
-//
-$l['acp.settings.form.Longitude'] = 'Längengrad';
-//
-$l['acp.settings.form.Longitude.decription'] = 'Geodaten für die Berechnung von Sonnenauf- und untergang';
-//
-$l['acp.settings.form.fbAddress'] = 'Adresse';
-//
-$l['acp.settings.form.fbAddress.decription'] = 'Host oder IP Adresse der Fritz!Box';
-//
-$l['acp.settings.form.5GHzWlan'] = '5GHz Wlan';
-//
-$l['acp.settings.form.5GHzWlan.decription'] = 'Hat die Fritz!Box ein 5GHz Wlan?';
-//
-$l['acp.settings.form.fbUser'] = 'Benutzer';
-//
-$l['acp.settings.form.fbUser.decription'] = 'Benutzername des Fritz!Box Benutzers';
-//
-$l['acp.settings.form.fbPassword'] = 'Passwort';
-//
-$l['acp.settings.form.fbPassword.decription'] = 'Passwort des Fritz!Box Benutzers';
-//
-$l['acp.settings.form.redirectActive'] = 'Umleitung Aktiv';
-//
-$l['acp.settings.form.redirectActive.decription'] = 'ist die Umleitung anktiv werden PCs, Tablets und Smartphones auf die EIngestellten Oberflächen umgeleitet';
-//
-$l['acp.settings.form.redirectPcTo'] = 'PC Umleitung';
-//
-$l['acp.settings.form.redirectPcTo.decription'] = 'legt die Oberfläche fest auf die ein PC umgeleitet wird';
-//
-$l['acp.settings.form.redirectTabletTo'] = 'Tablet Umleitung';
-//
-$l['acp.settings.form.redirectTabletTo.decription'] = 'legt die Oberfläche fest auf die ein Tablet umgeleitet wird';
-//
-$l['acp.settings.form.redirectSmartphoneTo'] = 'Smartphone Umleitung';
-//
-$l['acp.settings.form.redirectSmartphoneTo.decription'] = 'legt die Oberfläche fest auf die ein Smartphone umgeleitet wird';
-//
-$l['acp.settings.form.showUsersAtHome'] = 'Benutzer zu Hause Anzeigen';
-//
-$l['acp.settings.form.showUsersAtHome.decription'] = 'wenn Aktiv werden die Benutzer zu Hause mit ihrem aktuellen Status auf der Startseite Angezeigt';
+//$l['acp.settings.form.defaultStyle.decription'] = 'der hier eingestellte Webstyle gilt für alle Gäste und Benutzer die nicht selbst einen Style gewählt haben';
+$l['acp.settings.form.defaultStyle.decription'] = 'the webstyle configured here applies for all guests and users who do not select a style itself';
+//$l['acp.settings.form.defaultLanguage'] = 'Sprache';
+$l['acp.settings.form.defaultLanguage'] = 'Language';
+//$l['acp.settings.form.defaultLanguage.decription'] = 'die hier eingestellte Sprache gilt für alle Gäste und Benutzer die nicht selbst ein Sprache gewählt haben';
+$l['acp.settings.form.defaultLanguage.decription'] = 'the language configured here applies for all guests and users who do not select a style itself';
+//$l['acp.settings.form.Timezone'] = 'Zeitzone';
+$l['acp.settings.form.Timezone'] = 'Timezone';
+//$l['acp.settings.form.Timezone.decription'] = 'Zeitzone';
+$l['acp.settings.form.Timezone.decription'] = 'Timezone';
+//$l['acp.settings.form.defaultDateFormat'] = 'Datumsformat';
+$l['acp.settings.form.defaultDateFormat'] = 'Date format';
+//$l['acp.settings.form.defaultDateFormat.decription'] = 'Standard Datumsformat, die Bedutung der Formatierung ist in der PHP Funktionsbeschreibung der "date" Funktion zu finden';
+$l['acp.settings.form.defaultDateFormat.decription'] = 'Default date format, the meaning of the formatting is in the PHP function description of function  "date"';
+//$l['acp.settings.form.defaultTimeFormat'] = 'Zeitformat';
+$l['acp.settings.form.defaultTimeFormat'] = 'Time format';
+//$l['acp.settings.form.defaultTimeFormat.decription'] = 'Standard Zeitformat, die Bedutung der Formatierung ist in der PHP Funktionsbeschreibung der "date" Funktion zu finden';
+$l['acp.settings.form.defaultTimeFormat.decription'] = 'Default time format, the meaning of the formatting is in the PHP function description of function  "date"';
+//$l['acp.settings.form.useTimeline'] = 'Zeitanzeige';
+$l['acp.settings.form.useTimeline'] = 'Time display';
+//$l['acp.settings.form.useTimeline.decription'] = 'legt fest wie Datum un Urzeit angezeigt werden sollen';
+$l['acp.settings.form.useTimeline.decription'] = 'specifies how the date and time have to be displayed';
+//$l['acp.settings.form.sunriseOffset'] = 'Offset Sonnenaufgang';
+$l['acp.settings.form.sunriseOffset'] = 'Offset sunrise';
+//$l['acp.settings.form.sunriseOffset.decription'] = 'mit dieser Einstellung kann der errechnete Sonnenaufgang verschoben werden (heutiger Sonnenaufgang mit Offset: {1:s} Uhr)';
+$l['acp.settings.form.sunriseOffset.decription'] = 'with this setting, the calculated sunrise can be moved (today's sunrise with Offset: {1: s} clock)';
+//$l['acp.settings.form.sunsetOffset'] = 'Offset Sonnenuntergang';
+$l['acp.settings.form.sunsetOffset'] = 'Offset sunset';
+//$l['acp.settings.form.sunsetOffset.decription'] = 'mit dieser Einstellung kann der errechnete Sonnenuntergang verschoben werden (heutiger Sonnenuntergang mit Offset: {1:s} Uhr)';
+$l['acp.settings.form.sunsetOffset.decription'] = 'with this setting, the calculated sunset can be moved (today's sunrise with Offset: {1: s} clock)';
+//$l['acp.settings.form.Latitude'] = 'Breitengrad';
+$l['acp.settings.form.Latitude'] = 'Degree of latitude';
+//$l['acp.settings.form.Latitude.decription'] = 'Geodaten für die Berechnung von Sonnenauf- und untergang';
+$l['acp.settings.form.Latitude.decription'] = 'Geo data for the calculation of sunrise and sunset';
+//$l['acp.settings.form.Longitude'] = 'Längengrad';
+$l['acp.settings.form.Longitude'] = 'Degree of longitude';
+//$l['acp.settings.form.Longitude.decription'] = 'Geodaten für die Berechnung von Sonnenauf- und untergang';
+$l['acp.settings.form.Longitude.decription'] = 'Geo data for the calculation of sunrise and sunset';
+//$l['acp.settings.form.fbAddress'] = 'Adresse';
+$l['acp.settings.form.fbAddress'] = 'Adress';
+//$l['acp.settings.form.fbAddress.decription'] = 'Host oder IP Adresse der Fritz!Box';
+$l['acp.settings.form.fbAddress.decription'] = 'Host or IP address of the Fritz!Box';
+//$l['acp.settings.form.5GHzWlan'] = '5GHz Wlan';
+$l['acp.settings.form.5GHzWlan'] = '5GHz WLAN ';
+//$l['acp.settings.form.5GHzWlan.decription'] = 'Hat die Fritz!Box ein 5GHz Wlan?';
+$l['acp.settings.form.5GHzWlan.decription'] = 'Does the Fritz!Box have a 5GHz WLAN?';
+//$l['acp.settings.form.fbUser'] = 'Benutzer';
+$l['acp.settings.form.fbUser'] = 'User';
+//$l['acp.settings.form.fbUser.decription'] = 'Benutzername des Fritz!Box Benutzers';
+$l['acp.settings.form.fbUser.decription'] = 'Username of the Fritz!Box user';
+//$l['acp.settings.form.fbPassword'] = 'Passwort';
+$l['acp.settings.form.fbPassword'] = 'Password';
+//$l['acp.settings.form.fbPassword.decription'] = 'Passwort des Fritz!Box Benutzers';
+$l['acp.settings.form.fbPassword.decription'] = 'Password of the Fritz!Box user';
+//$l['acp.settings.form.redirectActive'] = 'Umleitung Aktiv';
+$l['acp.settings.form.redirectActive'] = 'Redirection active;
+//$l['acp.settings.form.redirectActive.decription'] = 'ist die Umleitung anktiv werden PCs, Tablets und Smartphones auf die EIngestellten Oberflächen umgeleitet';
+$l['acp.settings.form.redirectActive.decription'] = 'is the redirection active PCs, tablets and smartphones are redirected to the recruited surfaces';
+//$l['acp.settings.form.redirectPcTo'] = 'PC Umleitung';
+$l['acp.settings.form.redirectPcTo'] = 'PC redirection';
+//$l['acp.settings.form.redirectPcTo.decription'] = 'legt die Oberfläche fest auf die ein PC umgeleitet wird';
+$l['acp.settings.form.redirectPcTo.decription'] = 'Set the surface on the PC will be redirected';
+//$l['acp.settings.form.redirectTabletTo'] = 'Tablet Umleitung';
+$l['acp.settings.form.redirectTabletTo'] = 'Tablet redirection';
+//$l['acp.settings.form.redirectTabletTo.decription'] = 'legt die Oberfläche fest auf die ein Tablet umgeleitet wird';
+$l['acp.settings.form.redirectTabletTo.decription'] = 'Set the surface on the tablet will be redirected';
+//$l['acp.settings.form.redirectSmartphoneTo'] = 'Smartphone Umleitung';
+$l['acp.settings.form.redirectSmartphoneTo'] = 'Smartphone redirection';
+//$l['acp.settings.form.redirectSmartphoneTo.decription'] = 'legt die Oberfläche fest auf die ein Smartphone umgeleitet wird';
+$l['acp.settings.form.redirectSmartphoneTo.decription'] = 'Set the surface on the smartphone will be redirected;
+//$l['acp.settings.form.showUsersAtHome'] = 'Benutzer zu Hause Anzeigen';
+$l['acp.settings.form.showUsersAtHome'] = 'Display users at home';
+//$l['acp.settings.form.showUsersAtHome.decription'] = 'wenn Aktiv werden die Benutzer zu Hause mit ihrem aktuellen Status auf der Startseite Angezeigt';
+$l['acp.settings.form.showUsersAtHome.decription'] = 'if active users are displayed at home with their current status on the start page';
