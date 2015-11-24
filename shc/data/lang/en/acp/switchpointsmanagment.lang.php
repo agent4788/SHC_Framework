4fee730b8855522717521cd3b53ba861c68edbd2
Dietmar-Franken <d.franken@umsons.de>
2015-11-24 01:41:32 +0100

    Create switchpointsmanagment.lang.php

---
+++ shc/data/lang/en/acp/switchpointsmanagment.lang.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Schaltpunkteverwaltung Sprachvariablen
+ *
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+$l = array();
+
+//Allgemein
+$l['acp.switchpointsManagment.title'] = 'Schaltpunkte verwalten';
+$l['acp.switchpointsManagment.title.info'] = 'Info';
+$l['acp.switchpointsManagment.title.conditions'] = 'Bedingungen';
+$l['acp.switchpointsManagment.title.switchables'] = 'schaltbare Elemente';
+$l['acp.switchpointsManagment.title.addCondition'] = 'Bedingung hinzufügen';
+$l['acp.switchpointsManagment.title.addSwitchable'] = 'schaltbares Element hinzufügen';
+
+//Schaltpunkte Tabelle
+$l['acp.switchpointsManagment.switchPointList.table.head.name'] = 'Name';
+$l['acp.switchpointsManagment.switchPointList.table.head.lastExecute'] = 'letzte Ausführung';
+$l['acp.switchpointsManagment.switchPointList.table.head.lastExecute.never'] = 'nie Ausgeführt';
+$l['acp.switchpointsManagment.switchPointList.table.head.type'] = 'Typ';
+$l['acp.switchpointsManagment.switchPointList.table.head.command'] = 'Befehl';
+
+//Tooltip
+$l['acp.switchpointsManagment.tooltip.command'] = 'Befehl';
+$l['acp.switchpointsManagment.tooltip.condition'] = 'Bedingungen';
+$l['acp.switchpointsManagment.tooltip.condition.none'] = 'keine';
+$l['acp.switchpointsManagment.tooltip.year'] = 'Jahr';
+$l['acp.switchpointsManagment.tooltip.year.every'] = 'jedes Jahr';
+$l['acp.switchpointsManagment.tooltip.month'] = 'Monat';
+$l['acp.switchpointsManagment.tooltip.month.every'] = 'jeden Monat';
+$l['acp.switchpointsManagment.tooltip.day'] = 'Tag';
+$l['acp.switchpointsManagment.tooltip.day.every'] = 'jeden Tag';
+$l['acp.switchpointsManagment.tooltip.hour'] = 'Stunde';
+$l['acp.switchpointsManagment.tooltip.hour.every'] = 'jede Stunde';
+$l['acp.switchpointsManagment.tooltip.minute'] = 'Minute';
+$l['acp.switchpointsManagment.tooltip.minute.every'] = 'jede Minute';
+
+//Buttons
+$l['acp.switchpointsManagment.button.addSwitchPoint'] = 'neuer Schaltpunkt';
+$l['acp.switchpointsManagment.button.addSwitchPoint.extendetForm'] = 'erweitert';
+
+//Prompts
+$l['acp.switchpointsManagment.prompt.deleteSwitchPoint.title'] = 'Schaltpunkt löschen';
+$l['acp.switchpointsManagment.prompt.deleteSwitchPoint'] = 'Willst du den Schaltpunkt wirklich löschen?';
+
+//Formulate
+$l['acp.switchpointsManagment.form.switchPoint.name'] = 'Name';
+$l['acp.switchpointsManagment.form.switchPoint.name.decription'] = 'Name des Schaltpunkes';
+$l['acp.switchpointsManagment.form.switchPoint.command'] = 'Befehl';
+$l['acp.switchpointsManagment.form.switchPoint.command.decription'] = 'Befehl der ausgeführt werden soll wenn der Zeitpunkt des Schaltpunktes zurifft';
+$l['acp.switchpointsManagment.form.switchPoint.dayOfWeek'] = 'Tage';
+$l['acp.switchpointsManagment.form.switchPoint.dayOfWeek.decription'] = 'an denen der Befehl der ausgeführt werden soll';
+$l['acp.switchpointsManagment.form.switchPoint.dayOfWeek.val1'] = 'jeden Tag';
+$l['acp.switchpointsManagment.form.switchPoint.dayOfWeek.val2'] = 'jeden Wochentag';
+$l['acp.switchpointsManagment.form.switchPoint.dayOfWeek.val3'] = 'Wochenende';
+$l['acp.switchpointsManagment.form.switchPoint.condition'] = 'Bedingungen';
+$l['acp.switchpointsManagment.form.switchPoint.condition.decription'] = 'Bedingungen die zutreffen müssen damit der Schaltpunkt ausgeführt werden soll';
+$l['acp.switchpointsManagment.form.switchPoint.year.every'] = 'jedes Jahr';
+$l['acp.switchpointsManagment.form.switchPoint.year'] = 'Jahr';
+$l['acp.switchpointsManagment.form.switchPoint.year.decription'] = 'Jahr in dem der Schaltpunkt ausgeführt werden soll';
+$l['acp.switchpointsManagment.form.switchPoint.month.every'] = 'jeden Monat';
+$l['acp.switchpointsManagment.form.switchPoint.month'] = 'Monat';
+$l['acp.switchpointsManagment.form.switchPoint.month.decription'] = 'Monat in dem der Schaltpunkt ausgeführt werden soll';
+$l['acp.switchpointsManagment.form.switchPoint.day.every'] = 'jeden Tag';
+$l['acp.switchpointsManagment.form.switchPoint.day'] = 'Tag';
+$l['acp.switchpointsManagment.form.switchPoint.day.decription'] = 'Tag an dem der Schaltpunkt ausgeführt werden soll';
+$l['acp.switchpointsManagment.form.switchPoint.hour.every'] = 'jede Stunde';
+$l['acp.switchpointsManagment.form.switchPoint.hour'] = 'Stunde';
+$l['acp.switchpointsManagment.form.switchPoint.hour.decription'] = 'Stunde in der der Schaltpunkt ausgeführt werden soll';
+$l['acp.switchpointsManagment.form.switchPoint.minute.every'] = 'jede Minute';
+$l['acp.switchpointsManagment.form.switchPoint.minute'] = 'Minute';
+$l['acp.switchpointsManagment.form.switchPoint.minute.decription'] = 'Minute zu der der Schaltpunkt ausgeführt werden soll';
+$l['acp.switchpointsManagment.form.switchPoint.active'] = 'Aktiviert';
+$l['acp.switchpointsManagment.form.switchPoint.active.decription'] = 'Aktiviert/deaktiviert den Schaltpunkt';
+
+//Meldungen
+$l['acp.switchpointsManagment.form.success.switchPoint'] =  'Der Schaltpunkt wurde erfolgreich erstellt';
+$l['acp.switchpointsManagment.form.success.delete'] =  'Der Schaltpunkt wurde erfolgreich gelöscht';
+
+$l['acp.switchpointsManagment.form.error.id'] =  'Ungültige ID';
+
+$l['acp.switchpointsManagment.form.switchPoint.error.1503'] =  'Der Name des Schaltpunktes ist schon vergeben';
+$l['acp.switchpointsManagment.form.switchPoint.error.1102'] =  'Der Schaltpunkt konnte wegen fehlender Schreibrechte nicht gespeichert werden';
+$l['acp.switchpointsManagment.form.switchPoint.error'] =  'Der Schaltpunkt konnte nicht gespeichert werden';
+$l['acp.switchpointsManagment.form.error.1102.del'] =  'Der Schaltpunkt konnte wegen fehlender Schreibrechte nicht gelöscht werden';
+$l['acp.switchpointsManagment.form.error.del'] =  'Der Schaltpunkt konnte nicht gelöscht werden';
+
+$l['acp.switchpointsManagment.form.addCondition.success'] =  'Die Bedingung wurde erfolgreich hinzugefügt';
+$l['acp.switchpointsManagment.form.addCondition.error.1102'] =  'Die Bedingung konnte wegen fehlender Schreibrechte nicht gespeichert werden';
+$l['acp.switchpointsManagment.form.addCondition.error'] =  'Die Bedingung konnte nicht gespeichert werden';
+$l['acp.switchpointsManagment.form.addCondition.success.delete'] =  'Die Bedingung  wurde erfolgreich gelöscht';
+$l['acp.switchpointsManagment.form.addCondition.success'] =  'Die Bedingung wurde erfolgreich hinzugefügt';
+$l['acp.switchpointsManagment.form.addCondition.removeError.1102'] =  'Die Bedingung konnte wegen fehlender Schreibrechte nicht gelöscht werden';
+$l['acp.switchpointsManagment.form.addCondition.removeError'] =  'Die Bedingung konnte nicht gelöscht werden';
+$l['acp.switchpointsManagment.form.addElement.success'] =  'Das Element wurde erfolgreich gespeichert';
+$l['acp.switchpointsManagment.form.addElement.error.1102'] =  'Das Element konnte wegen fehlender schreibrechte nicht gespeichert werden';
+$l['acp.switchpointsManagment.form.addElement.error'] =  'Das Element konnte nicht gespeichert werden';
+$l['acp.switchpointsManagment.form.addElement.removeSuccesss'] =  'Das Element wurde erfolgreich glöscht';
+$l['acp.switchpointsManagment.form.addElement.removeError.1102'] =  'Das Element konnte wegen fehlender schreibrechte nicht glöscht werden';
+$l['acp.switchpointsManagment.form.addElement.removeError'] =  'Das Element konnte nicht glöscht werden';
