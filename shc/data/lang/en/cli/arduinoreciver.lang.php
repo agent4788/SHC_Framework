0e8ad7001c11dbe6ca440cf3543451ed1a7bb77a
Dietmar-Franken <d.franken@umsons.de>
2015-11-24 01:54:16 +0100

    Create arduinoreciver.lang.php

---
+++ shc/data/lang/en/cli/arduinoreciver.lang.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Sprachvariablen fuer den Arduino Reciver
+ * 
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+$l = array();
+
+//Eingaben
+$l['arduinoReciver.input.active'] = 'Arduino Empfänger Dienst aktiviert ({1:s}): ';
+$l['arduinoReciver.input.active.invalid'] = 'Ungültige Eingabe';
+$l['arduinoReciver.input.active.invalid.repeated'] = 'du hast zu oft eine ungültige Angaben eingegeben';
+$l['arduinoReciver.input.ip'] = 'IP Adresse des Sensor Empfängers ({1:s}): ';
+$l['arduinoReciver.input.ip.invalid'] = 'ungültige IP Adresse!';
+$l['arduinoReciver.input.ip.invalid.repeated'] = 'du hast zu oft eine ungültige Adressen eingegeben';
+$l['arduinoReciver.input.port'] = 'Port des Sensor Empfängers ({1:s}): ';
+$l['arduinoReciver.input.port.invalid'] = 'ungültiger Port';
+$l['arduinoReciver.input.port.invalid.repeated'] = 'du hast zu oft einen ungültigen Ports eingegeben';
+$l['arduinoReciver.input.interface'] = 'Serielle Schnittstelle: ';
+$l['arduinoReciver.input.interface.invalid'] = 'ungültige Eingabe';
+$l['arduinoReciver.input.interface.invalid.repeated'] = 'du hast zu oft eine ungültige Angabe eingegeben';
+$l['arduinoReciver.input.interface.active'] = 'aktiv';
+$l['arduinoReciver.input.interface.active.notAvailable'] = 'aktiv (aber nicht verfügbar)';
+
+//Meldungen
+$l['arduinoReciver.inactive'] = 'Der Arduino Reciver wurde deaktiviert';
+
+$l['arduinoReciver.input.save.success'] = 'Die Einstellungen wurden erfolgreich gespeichert und werden nach dem nächsten neustart des Servers aktiv';
+$l['arduinoReciver.input.save.error'] = 'Die Einstellungen konnten nicht gespeichert werden';
+
