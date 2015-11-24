49b522d787c610abfa6b9ede64572a8e98f6d556
Dietmar-Franken <d.franken@umsons.de>
2015-11-24 16:05:30 +0100

    Update global.lang.php

--- shc/data/lang/en/global.lang.php
+++ shc/data/lang/en/global.lang.php
@@ -45,7 +45,7 @@ $l['global.stop'] = 'stop';
 //$l['global.toggle'] = 'umschalten';
 $l['global.toggle'] = 'toggle';
 //$l['global.accessDenied'] = 'Zugriff verweigert';
-$l['global.accessDenied'] = 'Zugriff verweigert';
+$l['global.accessDenied'] = 'Access denied';
 
 //Datum
 //Date
@@ -59,133 +59,133 @@ $l['global.date.now'] = 'just now';
 //$l['global.date.yesterday'] = 'gestern';
 $l['global.date.yesterday'] = 'yesterday';
 //$l['global.date.previousDays'] = 'vor {1:i} Tagen';
-$l['global.date.previousDays'] = 'vor {1:i} Tagen';
-//
-$l['global.date.nextDaysAgo'] = 'in {1:i} Tagen';
-//
-$l['global.date.oneHourAgo'] = 'vor einer Stunde';
-//
-$l['global.date.viewHoursAgo'] = 'vor {1:i} Stunden';
-//
-$l['global.date.oneMinuteAgo'] = 'vor einer Minute';
-//
-$l['global.date.viewMinutesAgo'] = 'vor {1:i} Minuten';
-//
-$l['global.date.oneSecondAgo'] = 'vor einer Sekunde';
-//
-$l['global.date.viewSecondsAgo'] = 'vor {1:i} Sekunden';
-//
-$l['global.date.oneHour'] = 'in einer Stunde';
-//
-$l['global.date.viewHours'] = 'in {1:i} Stunden';
-//
-$l['global.date.oneMinute'] = 'in einer Minute';
-//
-$l['global.date.viewMinutes'] = 'in {1:i} Minuten';
-//
-$l['global.date.oneSecond'] = 'in einer Sekunde';
-//
-$l['global.date.viewSeconds'] = 'in {1:i} Sekunden';
+$l['global.date.previousDays'] = 'before {1:i} Days';
+//$l['global.date.nextDaysAgo'] = 'in {1:i} Tagen';
+$l['global.date.nextDaysAgo'] = 'in {1:i} Days';
+//$l['global.date.oneHourAgo'] = 'vor einer Stunde';
+$l['global.date.oneHourAgo'] = 'before an hour';
+//$l['global.date.viewHoursAgo'] = 'vor {1:i} Stunden';
+$l['global.date.viewHoursAgo'] = 'before {1:i} Hours';
+//$l['global.date.oneMinuteAgo'] = 'vor einer Minute';
+$l['global.date.oneMinuteAgo'] = 'before an minute';
+//$l['global.date.viewMinutesAgo'] = 'vor {1:i} Minuten';
+$l['global.date.viewMinutesAgo'] = 'before {1:i} minutes';
+//$l['global.date.oneSecondAgo'] = 'vor einer Sekunde';
+$l['global.date.oneSecondAgo'] = 'before an second';
+//$l['global.date.viewSecondsAgo'] = 'vor {1:i} Sekunden';
+$l['global.date.viewSecondsAgo'] = 'before {1:i} seconds';
+//$l['global.date.oneHour'] = 'in einer Stunde';
+$l['global.date.oneHour'] = 'in one hour';
+//$l['global.date.viewHours'] = 'in {1:i} Stunden';
+$l['global.date.viewHours'] = 'in {1:i} hours';
+//$l['global.date.oneMinute'] = 'in einer Minute';
+$l['global.date.oneMinute'] = 'in one minute';
+//$l['global.date.viewMinutes'] = 'in {1:i} Minuten';
+$l['global.date.viewMinutes'] = 'in {1:i} minutes';
+//$l['global.date.oneSecond'] = 'in einer Sekunde';
+$l['global.date.oneSecond'] = 'in one second';
+//$l['global.date.viewSeconds'] = 'in {1:i} Sekunden';
+$l['global.date.viewSeconds'] = 'in {1:i} seconds';
 
 //Allgemeine Buttons
 //General Buttons
 
-//
-$l['global.button.back'] = 'zurück';
-//
-$l['global.button.save'] = 'speichern';
-//
-$l['global.button.edit'] = 'bearbeiten';
-//
-$l['global.button.editlist'] = 'Liste bearbeiten';
-//
-$l['global.button.delete'] = 'löschen';
-//
+//$l['global.button.back'] = 'zurück';
+$l['global.button.back'] = 'back';
+//$l['global.button.save'] = 'speichern';
+$l['global.button.save'] = 'save';
+//$l['global.button.edit'] = 'bearbeiten';
+$l['global.button.edit'] = 'edit';
+//$l['global.button.editlist'] = 'Liste bearbeiten'
+$l['global.button.editlist'] = 'edit list';
+//$l['global.button.delete'] = 'löschen';
+$l['global.button.delete'] = 'delete';
+//$l['global.button.yes'] = $l['global.yes'];
 $l['global.button.yes'] = $l['global.yes'];
-//
+//$l['global.button.no'] = $l['global.no'];
 $l['global.button.no'] = $l['global.no'];
-//
-$l['global.button.reboot'] = 'Neustart';
-//
-$l['global.button.shutdown'] = 'Herunterfahren';
+//$l['global.button.reboot'] = 'Neustart';
+$l['global.button.reboot'] = 'Reboot';
+//$l['global.button.shutdown'] = 'Herunterfahren';
+$l['global.button.shutdown'] = 'Shutdown';
 
 //globale Prompts
 //global prompts
 
-//
-$l['global.prompt.reboot.title'] = 'Neustart';
-//
-$l['global.prompt.reboot.text'] = 'willst du den Raspberry Pi wirklich neu starten?';
-//
-$l['global.prompt.shutdown.title'] = 'Herunterfahren';
-//
-$l['global.prompt.shutdown.text'] = 'willst du den Raspberry Pi wirklich Herunterfahren?';
+//$l['global.prompt.reboot.title'] = 'Neustart';
+$l['global.prompt.reboot.title'] = 'Reboot';
+//$l['global.prompt.reboot.text'] = 'willst du den Raspberry Pi wirklich neu starten?'
+$l['global.prompt.reboot.text'] = 'do you really want to restart the Raspberry Pi?';
+//$l['global.prompt.shutdown.title'] = 'Herunterfahren';
+$l['global.prompt.shutdown.title'] = 'Shutdown';
+//$l['global.prompt.shutdown.text'] = 'willst du den Raspberry Pi wirklich Herunterfahren?';
+$l['global.prompt.shutdown.text'] = 'do you really want to shutdown the Raspberry Pi?';
 
 //Datum
 //Date
 
-//
-$l['global.date.month.1'] = 'Januar';
-//
-$l['global.date.month.2'] = 'Februar';
-//
-$l['global.date.month.3'] = 'März';
-//
+//$l['global.date.month.1'] = 'Januar';
+$l['global.date.month.1'] = 'January';
+//$l['global.date.month.2'] = 'Februar';
+$l['global.date.month.2'] = 'February';
+//$l['global.date.month.3'] = 'März';
+$l['global.date.month.3'] = 'March';
+//$l['global.date.month.4'] = 'April';
 $l['global.date.month.4'] = 'April';
-//
-$l['global.date.month.5'] = 'Mai';
-//
-$l['global.date.month.6'] = 'Juni';
-//
-$l['global.date.month.7'] = 'Juli';
-//
+//$l['global.date.month.5'] = 'Mai';
+$l['global.date.month.5'] = 'May';
+//$l['global.date.month.6'] = 'Juni';
+$l['global.date.month.6'] = 'June';
+//$l['global.date.month.7'] = 'Juli';
+$l['global.date.month.7'] = 'July';
+//$l['global.date.month.8'] = 'August';
 $l['global.date.month.8'] = 'August';
-//
+//$l['global.date.month.9'] = 'September';
 $l['global.date.month.9'] = 'September';
-//
-$l['global.date.month.10'] = 'Oktober';
-//
+//$l['global.date.month.10'] = 'Oktober';
+$l['global.date.month.10'] = 'October';
+//$l['global.date.month.11'] = 'November';
 $l['global.date.month.11'] = 'November';
-//
-$l['global.date.month.12'] = 'Dezember';
-//
+//$l['global.date.month.12'] = 'Dezember';
+$l['global.date.month.12'] = 'December';
+//$l['global.date.month.1.short'] = 'Jan';
 $l['global.date.month.1.short'] = 'Jan';
-//
+//$l['global.date.month.2.short'] = 'Feb';
 $l['global.date.month.2.short'] = 'Feb';
-//
-$l['global.date.month.3.short'] = 'März';
-//
+//$l['global.date.month.3.short'] = 'Mär';
+$l['global.date.month.3.short'] = 'Mar';
+//$l['global.date.month.4.short'] = 'Apr';
 $l['global.date.month.4.short'] = 'Apr';
-//
-$l['global.date.month.5.short'] = 'Mai';
-//
+//$l['global.date.month.5.short'] = 'Mai';
+$l['global.date.month.5.short'] = 'May';
+//$l['global.date.month.6.short'] = 'Jun';
 $l['global.date.month.6.short'] = 'Jun';
-//
+//$l['global.date.month.7.short'] = 'Jul';
 $l['global.date.month.7.short'] = 'Jul';
-//
+//$l['global.date.month.8.short'] = 'Aug';
 $l['global.date.month.8.short'] = 'Aug';
-//
+//$l['global.date.month.9.short'] = 'Sep';
 $l['global.date.month.9.short'] = 'Sep';
-//
-$l['global.date.month.10.short'] = 'Okt';
-//
+//$l['global.date.month.10.short'] = 'Okt';
+$l['global.date.month.10.short'] = 'Oct';
+//$l['global.date.month.11.short'] = 'Nov';
 $l['global.date.month.11.short'] = 'Nov';
-//
-$l['global.date.month.12.short'] = 'Dez';
-//
-$l['global.date.weekDay.mon'] = 'Montag';
-//
-$l['global.date.weekDay.tue'] = 'Dienstag';
-//
-$l['global.date.weekDay.wed'] = 'Mittwoch';
-//
-$l['global.date.weekDay.thu'] = 'Donnerstag';
+//$l['global.date.month.12.short'] = 'Dez';
+$l['global.date.month.12.short'] = 'Dec';
+//$l['global.date.weekDay.mon'] = 'Montag';
+$l['global.date.weekDay.mon'] = 'Monday';
+//$l['global.date.weekDay.tue'] = 'Dienstag';
+$l['global.date.weekDay.tue'] = 'Tuesday';
+//$l['global.date.weekDay.wed'] = 'Mittwoch';
+$l['global.date.weekDay.wed'] = 'Wednesday';
+//$l['global.date.weekDay.thu'] = 'Donnerstag';
+$l['global.date.weekDay.thu'] = 'Thursday';
 //$l['global.date.weekDay.fri'] = 'Freitag';
 $l['global.date.weekDay.fri'] = 'Friday';
 //$l['global.date.weekDay.sat'] = 'Samstag';
 $l['global.date.weekDay.sat'] = 'Saturday';
-//$l['global.date.weekDay.sun'] = 'Sunday';
-$l['global.date.weekDay.sun'] = 'Sonntag';
+//$l['global.date.weekDay.sun'] = 'Sonntag';
+$l['global.date.weekDay.sun'] = 'Sunday';
 //$l['global.date.weekDay.mon.short'] = 'Mo';
 $l['global.date.weekDay.mon.short'] = 'Mo';
 //$l['global.date.weekDay.tue.short'] = 'Di';
@@ -200,20 +200,20 @@ $l['global.date.weekDay.fri.short'] = 'Fr';
 $l['global.date.weekDay.sat.short'] = 'Sa';
 //$l['global.date.weekDay.sun.short'] = 'So';
 $l['global.date.weekDay.sun.short'] = 'Su';
-//$l['global.date.time.jear'] = 'Year';
-$l['global.date.time.jear'] = 'Jahr';
+//$l['global.date.time.jear'] = 'Jahr';
+$l['global.date.time.jear'] = 'Year';
 //$l['global.date.time.jears'] = 'Jahre';
-$l['global.date.time.jears'] = 'YearsJ';
+$l['global.date.time.jears'] = 'Years';
 //$l['global.date.time.month'] = 'Monat';
 $l['global.date.time.month'] = 'Month';
 //$l['global.date.time.months'] = 'Monate';
 $l['global.date.time.months'] = 'Months';
 //$l['global.date.time.week'] = 'Woche';
 $l['global.date.time.week'] = 'Week';
-//$l['global.date.time.weeks'] = 'Weeks';
-$l['global.date.time.weeks'] = 'Wochen';
-//$l['global.date.time.day'] = 'Day';
-$l['global.date.time.day'] = 'Tag';
+//$l['global.date.time.weeks'] = 'Wochen';
+$l['global.date.time.weeks'] = 'Weeks';
+//$l['global.date.time.day'] = 'Tag';
+$l['global.date.time.day'] = 'Day';
 //$l['global.date.time.days'] = 'Tage';
 $l['global.date.time.days'] = 'Days';
 //$l['global.date.time.hour'] = 'Stunde';
