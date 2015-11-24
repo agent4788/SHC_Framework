e56f6bb3d480a1b2aaf8ee7c1c3172f9ecceded7
Dietmar-Franken <d.franken@umsons.de>
2015-11-24 01:47:18 +0100

    Create usersathomemanagement.lang.php

---
+++ shc/data/lang/en/acp/usersathomemanagement.lang.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Benutzer zu Hause verwaltung Sprachvariablen
+ *
+ * @author     Oliver Kleditzsch
+ * @copyright  Copyright (c) 2014, Oliver Kleditzsch
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since      2.0.0-0
+ * @version    2.0.0-0
+ */
+$l = array();
+
+//Allgemein
+$l['acp.usersathomeManagement.title'] = 'Benutzer zu Hause verwaltung';
+
+//Benutzeruebersicht
+$l['acp.usersathomeManagement.userlist.table.head.name'] = 'Name';
+$l['acp.usersathomeManagement.userlist.table.head.ip'] = 'IP Adresse';
+
+//Buttons
+$l['acp.usersathomeManagement.button.addUser'] = 'neuer Benutzer';
+
+//Prompts
+$l['acp.usersathomeManagement.prompt.deleteUser.title'] = 'Benutzer zu Hause löschen';
+$l['acp.usersathomeManagement.prompt.deleteUser'] = 'Willst du den Benutzer wirklich löschen?';
+
+//Formular
+$l['acp.usersathomeManagement.form.userAtHome.name'] = 'Name';
+$l['acp.usersathomeManagement.form.userAtHome.name.description'] = 'Name des Benutzers';
+$l['acp.usersathomeManagement.form.userAtHome.ip'] = 'IP Adresse';
+$l['acp.usersathomeManagement.form.userAtHome.ip.description'] = 'IP Adresse des Gerätes mit dem der Benutzer identifiziert wird';
+$l['acp.usersathomeManagement.form.userAtHome.visibility'] = 'Sichtbarkeit';
+$l['acp.usersathomeManagement.form.userAtHome.visibility.description'] = 'Aktiviert/Deaktiviert die Anzeige des Benutzers (der Benutzer wird wenn Deaktiviert nicht in der Benutzeroberfläche angezeigt)';
+$l['acp.usersathomeManagement.form.userAtHome.active'] = 'Aktiviert';
+$l['acp.usersathomeManagement.form.userAtHome.active.description'] = 'Aktiviert/Deaktiviert den Benutzer (Bedingungen und Ereignisse die den Benutzer betreffen werden nicht mehr ausgeführt)';
+
+//Meldungen
+$l['acp.usersathomeManagement.form.success.addUser'] =  'Der Benutzer wurde erfolgreich erstellt';
+$l['acp.usersathomeManagement.form.success.editUser'] =  'Der Benutzer wurde erfolgreich bearbeitet';
+$l['acp.usersathomeManagement.form.success.deleteUser'] =  'Der Benutzer wurde erfolgreich gelöscht';
+$l['acp.usersathomeManagement.form.success.orderUser'] =  'Die Sortierung wurde erfolgreich gespeichert';
+$l['acp.usersathomeManagement.form.error.1507'] =  'Der Name des Benutzers ist schon vergeben';
+$l['acp.usersathomeManagement.form.error.1102'] =  'Der Benutzer konnte wegen fehlender Schreibrechte nicht gespeichert werden';
+$l['acp.usersathomeManagement.form.error.1102.del'] =  'Der Benutzer konnte wegen fehlender Schreibrechte nicht gelöscht werden';
+$l['acp.usersathomeManagement.form.error'] =  'Der Benutzer konnte nicht gespeichert werden';
+$l['acp.usersathomeManagement.form.error.del'] =  'Der Benutzer konnte nicht gelöscht werden';
+$l['acp.usersathomeManagement.form.error.id'] =  'Ungültige ID';
+$l['acp.usersathomeManagement.form.error.orderUser'] =  'Die Sortierung konnte nicht gespeichert werden';
