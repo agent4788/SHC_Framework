dc9ab6a0d233a510e97fddec18e8e0e69538c8c2
Dietmar-Franken <d.franken@umsons.de>
2015-11-24 21:11:42 +0100

    Update sensortransmitterdaemon.lang.php

--- shc/data/lang/en/cli/sensortransmitterdaemon.lang.php
+++ shc/data/lang/en/cli/sensortransmitterdaemon.lang.php
@@ -12,24 +12,45 @@
 $l = array();
 
 //Eingaben
-$l['sensorTransmitter.input.active'] = 'Sensor Transmitter Dienst aktiviert ({1:s}): ';
-$l['sensorTransmitter.input.active.invalid'] = 'Ungültige Eingabe';
-$l['sensorTransmitter.input.active.invalid.repeated'] = 'du hast zu oft eine ungültige Angaben eingegeben';
-$l['sensorTransmitter.input.ip'] = 'IP Adresse des Sensor Empfängers ({1:s}): ';
-$l['sensorTransmitter.input.ip.invalid'] = 'ungültige IP Adresse!';
-$l['sensorTransmitter.input.ip.invalid.repeated'] = 'du hast zu oft eine ungültige Adressen eingegeben';
-$l['sensorTransmitter.input.port'] = 'Port des Sensor Empfängers ({1:s}): ';
-$l['sensorTransmitter.input.port.invalid'] = 'ungültiger Port';
-$l['sensorTransmitter.input.port.invalid.repeated'] = 'du hast zu oft einen ungültigen Ports eingegeben';
-$l['sensorTransmitter.input.sensorPointId'] = 'Sensor Punkt ID ({1:s}): ';
-$l['sensorTransmitter.input.sensorPointId.invalid'] = 'ungültige ID';
-$l['sensorTransmitter.input.sensorPointId.invalid.repeated'] = 'du hast zu oft eine ungültige ID eingegeben';
-$l['sensorTransmitter.input.sensorPointId.info'] = 'Die Sensor Punkt ID muss im gesamten Netzwerk eindeutig sein, über diese ID können die Sensoren einem Standort besser zugeordnet werden';
 
+
+//$l['sensorTransmitter.input.active'] = 'Sensor Transmitter Dienst aktiviert ({1:s}): ';
+$l['sensorTransmitter.input.active'] = 'Sensor transmitter server enabled ({1:s}): ';
+//$l['sensorTransmitter.input.active.invalid'] = 'Invalid input';
+$l['sensorTransmitter.input.active.invalid'] = 'Ungültige Eingabe';
+//$l['sensorTransmitter.input.active.invalid.repeated'] = 'du hast zu oft eine ungültige Angaben eingegeben';
+$l['sensorTransmitter.input.active.invalid.repeated'] = 'You have entered too many times an invalid data';
+//$l['sensorTransmitter.input.ip'] = 'IP Adresse des Sensor Empfängers ({1:s}): ';
+$l['sensorTransmitter.input.ip'] = 'IP Adress of sensor receiver ({1:s}): ';
+//$l['sensorTransmitter.input.ip.invalid'] = 'ungültige IP Adresse!';
+$l['sensorTransmitter.input.ip.invalid'] = 'invalid IP Adress!';
+//$l['sensorTransmitter.input.ip.invalid.repeated'] = 'du hast zu oft eine ungültige Adressen eingegeben';
+$l['sensorTransmitter.input.ip.invalid.repeated'] = 'You have entered too many times an invalid addresses';
+//$l['sensorTransmitter.input.port'] = 'Port des Sensor Empfängers ({1:s}): ';
+$l['sensorTransmitter.input.port'] = 'Port of sensor receiver ({1:s}): ';
+//$l['sensorTransmitter.input.port.invalid'] = 'ungültiger Port';
+$l['sensorTransmitter.input.port.invalid'] = 'ungültiger Port';
+//$l['sensorTransmitter.input.port.invalid.repeated'] = 'du hast zu oft einen ungültigen Ports eingegeben';
+$l['sensorTransmitter.input.port.invalid.repeated'] = 'You have entered too many times an invalid port';
+//$l['sensorTransmitter.input.sensorPointId'] = 'Sensor Punkt ID ({1:s}): ';
+$l['sensorTransmitter.input.sensorPointId'] = 'Sensor Point ID ({1:s}): ';
+//$l['sensorTransmitter.input.sensorPointId.invalid'] = 'ungültige ID';
+$l['sensorTransmitter.input.sensorPointId.invalid'] = 'invalid ID';
+//$l['sensorTransmitter.input.sensorPointId.invalid.repeated'] = 'du hast zu oft eine ungültige ID eingegeben';
+$l['sensorTransmitter.input.sensorPointId.invalid.repeated'] = 'You have entered too many times an invalid ID';
+//$l['sensorTransmitter.input.sensorPointId.info'] = 'Die Sensor Punkt ID muss im gesamten Netzwerk eindeutig sein, über diese ID können die Sensoren einem Standort besser zugeordnet werden';
+$l['sensorTransmitter.input.sensorPointId.info'] = 'The sensor point ID must be unique throughout the network, over this ID the sensor can be assigned better to a location';
+//$l['sensorTransmitter.input.blinkPin'] = 'Status LED Pin [-1 wenn deaktiviert] ({1:s}): ';
 $l['sensorTransmitter.input.blinkPin'] = 'Status LED Pin [-1 wenn deaktiviert] ({1:s}): ';
-$l['sensorTransmitter.input.blinkPin.invalid'] = 'Ungültige Eingabe';
-$l['sensorTransmitter.input.blinkPin.invalid.repeated'] = 'du hast zu oft eine ungültige Angaben eingegeben';
+//$l['sensorTransmitter.input.blinkPin.invalid'] = 'Ungültige Eingabe';
+$l['sensorTransmitter.input.blinkPin.invalid'] = 'invalid input';
+//$l['sensorTransmitter.input.blinkPin.invalid.repeated'] = 'du hast zu oft eine ungültige Angaben eingegeben';
+$l['sensorTransmitter.input.blinkPin.invalid.repeated'] = 'You have entered too many times an invalid data';
 
 //Meldungen
-$l['sensorTransmitter.input.save.success'] = 'Die Einstellungen wurden erfolgreich gespeichert und werden nach dem nächsten neustart des Servers aktiv';
-$l['sensorTransmitter.input.save.error'] = 'Die Einstellungen konnten nicht gespeichert werden';
+
+
+//$l['sensorTransmitter.input.save.success'] = 'Die Einstellungen wurden erfolgreich gespeichert und werden nach dem nächsten neustart des Servers aktiv';
+$l['sensorTransmitter.input.save.success'] = 'The settings have been successfully saved and will be active after the next reboot of the server';
+//$l['sensorTransmitter.input.save.error'] = 'Die Einstellungen konnten nicht gespeichert werden';
+$l['sensorTransmitter.input.save.error'] = 'The settings could not be saved';
